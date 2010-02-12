42392afb026fcc855e2ba6a89488c75830f5e937
lrose <lrose@b1e7822e-7b56-0410-88b3-db6ede196368>
2010-02-12 15:51:43 +0000

    [286050] EGL store is deprecated in favour of generate.

--- plugins/org.eclipse.epsilon.egl.engine/src/org/eclipse/epsilon/egl/types/EglFileGeneratingTemplate.java
+++ plugins/org.eclipse.epsilon.egl.engine/src/org/eclipse/epsilon/egl/types/EglFileGeneratingTemplate.java
@@ -21,127 +21,66 @@ import org.eclipse.epsilon.egl.execute.context.IEglContext;
 import org.eclipse.epsilon.egl.merge.output.ProtectedRegion;
 import org.eclipse.epsilon.egl.output.Writer;
 import org.eclipse.epsilon.egl.status.ProtectedRegionWarning;
-import org.eclipse.epsilon.egl.status.StatusMessage;
 import org.eclipse.epsilon.egl.traceability.OutputFile;
 import org.eclipse.epsilon.egl.util.FileUtil;
 
-public class EglFileGeneratingTemplate extends EglTemplate {
+public class EglFileGeneratingTemplate extends EglPersistentTemplate {
 
-	private final URI    outputRoot;
-	private final String outputRootPath;
-	
 	private String currentOutputPath;
 	private OutputFile currentOutputFile;
-	
+
 	// For tests
-	protected EglFileGeneratingTemplate(URI path,
-	                                    IEglContext callersContext,
-	                                    URI outputRoot) throws EglRuntimeException {
-		
+	protected EglFileGeneratingTemplate(URI path, IEglContext callersContext, URI outputRoot) throws EglRuntimeException {
 		this(path.toString(), path, callersContext, outputRoot, outputRoot.getPath());
 	}
-	
-	public EglFileGeneratingTemplate(String name,
-	                                 URI path, 
-	                                 IEglContext callersContext, 
-	                                 URI outputRoot,
-	                                 String outputRootPath) throws EglRuntimeException {
 
-			super(name, path, callersContext);
-			this.outputRoot     = outputRoot;
-			this.outputRootPath = outputRootPath;
+	public EglFileGeneratingTemplate(String name, URI path, IEglContext callersContext, URI outputRoot, String outputRootPath) throws EglRuntimeException {
+		super(name, path, callersContext, outputRoot, outputRootPath);
 	}
-	
-	private void addMessage(String message) {
-		callersContext.addStatusMessage(new StatusMessage(message));
-	}
-	
-	private String name(String path) {
-		String name = path;
-		 
-		 if (outputRootPath != null)
-			 name = new File(outputRootPath).getPath() + FileUtil.FILE_SEP + name;
-		 
-		 return name;
-	}
-	
-	private void write(File file, String contents, String message) throws EglRuntimeException {		
-		try {
-			new Writer(file, contents).write();
-		
-			addMessage(message);
-			
-			currentOutputFile = module.getContext().getTemplate().addOutputFile(name(currentOutputPath), UriUtil.fileToUri(file));
 
-		} catch (IOException ex) {
-			throw new EglRuntimeException("Could not write to " + file, ex, callersContext.getModule().getAst());
-		
-		} catch (URISyntaxException e) {
-			throw new EglRuntimeException("Could not resolve path: "+file, e, callersContext.getModule().getAst());
-		}
-	}
-	
-	protected File resolveFile(String path) throws EglRuntimeException {
+	protected void doGenerate(File target, String targetName, boolean overwrite, boolean protectRegions) throws EglRuntimeException {
+		currentOutputPath = target.getAbsolutePath();
+
 		try {
-			final String encodedPath = UriUtil.encode(path, false);
-			final URI resolved       = UriUtil.resolve(encodedPath, outputRoot);
-			
-			if ("file".equals(resolved.getScheme()))
-				return new File(resolved);
-			
-			else {
-				return new File(new URI("file://" + resolved.toString()));
-			}
-			
-		} catch (URISyntaxException e) {
-			throw new EglRuntimeException("Could not resolve path: "+path, e, callersContext.getModule().getAst());
-		}
-	}
-	
-	public void store(String path) throws EglRuntimeException {
-		store(path, false);
-	}
-	
-	public void store(String path, boolean overwrite) throws EglRuntimeException {
-		currentOutputPath = path;
-		final File outputFile = resolveFile(path);
-		
-		if (!overwrite && outputFile.exists()) {
-			addMessage("Existing contents of "+name(path)+" were preserved.");
-		
-		} else {
-			if (!isProcessed()) process();
-			write(outputFile, getContents(), "Successfully wrote to " + name(path));
-		}
-	}
-	
-	public void generate(String path) throws EglRuntimeException {
-		try {
-			currentOutputPath = path;
-			final File outputFile = resolveFile(path);
-			
-			if (!isProcessed()) process();
-			
-			if (outputFile.exists()) {
-				final String merged = merge(FileUtil.read(outputFile));
-				
-				write(outputFile, merged, "Protected regions of "+name(path)+" were preserved.");
-			
+			if (protectRegions && target.exists()) {
+				final String merged = merge(FileUtil.read(target));
+
+				write(target, targetName, merged, "Protected regions of " + targetName + " were preserved.");
+
 				for (ProtectedRegion pr : module.getContext().getPartitioner().partition(merged).getProtectedRegions()) {
 					currentOutputFile.addProtectedRegion(pr.getId(), pr.isEnabled(), pr.getOffset());
 				}
-				
+
 			} else {
-				write(outputFile, getContents(), "Successfully wrote to " + name(path));
+				write(target, targetName, getContents(), "Successfully wrote to " + targetName);
 			}
 		} catch (IOException ex) {
-			throw new EglRuntimeException("Could not generate to " + path, ex, callersContext.getModule().getAst());
+			throw new EglRuntimeException("Could not generate to " + targetName, ex, callersContext.getModule().getAst());
 		}
+
 	}
 	
+	private void write(File target, String targetName, String contents, String message) throws EglRuntimeException {
+		try {
+			new Writer(target, contents).write();
+
+			addMessage(message);
+
+			currentOutputFile = module.getContext().getTemplate().addOutputFile(targetName, UriUtil.fileToUri(target));
+
+		} catch (IOException ex) {
+			throw new EglRuntimeException("Could not write to " + target, ex,
+					callersContext.getModule().getAst());
+
+		} catch (URISyntaxException e) {
+			throw new EglRuntimeException("Could not resolve path: " + target, e,
+					callersContext.getModule().getAst());
+		}
+	}
+
 	@Override
 	protected void addProtectedRegionWarning(ProtectedRegionWarning warning) {
 		super.addProtectedRegionWarning(new ProtectedRegionWarning(warning.getId(), currentOutputPath));
 	}
-	
+
 }
---
+++ plugins/org.eclipse.epsilon.egl.engine/src/org/eclipse/epsilon/egl/types/EglPersistentTemplate.java
@@ -0,0 +1,160 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The University of York.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Louis Rose - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.epsilon.egl.types;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.eclipse.epsilon.common.dt.console.EpsilonConsole;
+import org.eclipse.epsilon.commons.util.UriUtil;
+import org.eclipse.epsilon.egl.exceptions.EglRuntimeException;
+import org.eclipse.epsilon.egl.execute.context.IEglContext;
+import org.eclipse.epsilon.egl.status.StatusMessage;
+import org.eclipse.epsilon.egl.util.FileUtil;
+import org.eclipse.ui.PlatformUI;
+
+public abstract class EglPersistentTemplate extends EglTemplate {
+
+	protected final URI outputRoot;
+	protected final String outputRootPath;
+
+	public EglPersistentTemplate(String name, URI path, IEglContext callersContext, URI outputRoot, String outputRootPath) throws EglRuntimeException {
+		super(name, path, callersContext);
+		
+		this.outputRoot     = outputRoot;
+		this.outputRootPath = outputRootPath;
+	}
+
+	protected File resolveFile(String path) throws EglRuntimeException {
+		try {
+			final String encodedPath = UriUtil.encode(path, false);
+			final URI resolved = UriUtil.resolve(encodedPath, outputRoot);
+
+			if ("file".equals(resolved.getScheme()))
+				return new File(resolved);
+
+			else {
+				return new File(new URI("file://" + resolved.toString()));
+			}
+
+		} catch (URISyntaxException e) {
+			throw new EglRuntimeException("Could not resolve path: " + path, e,
+					callersContext.getModule().getAst());
+		}
+	}
+
+	/**
+	 * This processes the template and stores the result in the given file,
+	 * provided that this file does not exist yet. This method is here for
+	 * backward compatibility only.
+	 * 
+	 * @deprecated Use generate(path) instead, or generate(path, false, false),
+	 *             if you really know what you are doing :-)
+	 * @param path
+	 * @throws EglRuntimeException
+	 */
+	public void store(String path) throws EglRuntimeException {
+		generate(path, false, false);
+		printStoreDeprecationWarning("store(path, overwrite)", "generate(path, false, false)");
+	}
+
+	/**
+	 * This processes the template and stores the result in the given file.
+	 * If overwrite is false, an existing file will prevent the result
+	 * from being written. If overwrite is true, any existing file will be
+	 * overwritten, not respecting protected regions. This method is here for
+	 * backward compatibility only.
+	 * 
+	 * @deprecated Use generate(path) instead, or generate(path, overwrite, false),
+	 *             if you really know what you are doing :-)
+	 * @param path
+	 * @param overwrite
+	 * @throws EglRuntimeException
+	 */
+	public void store(String path, boolean overwrite) throws EglRuntimeException {
+		generate(path, overwrite, false);
+		printStoreDeprecationWarning("store(path, overwrite)", "generate(path, overwrite, false)");
+	}
+
+	private void printStoreDeprecationWarning(String deprecatedCode, String replacementCode) {
+		final String message = deprecatedCode + " is deprecated and will be removed in a future version of EGL. " +
+		                       "generate(path), which respects protected regions, is preferred. " +
+		                       "Alternatively, use " + replacementCode + " if you really know what you are doing.";
+		
+		if (PlatformUI.isWorkbenchRunning()) {
+			EpsilonConsole.getInstance().getWarningStream().println(message);
+		} else {
+			System.err.println("Warning: " + message);
+		}
+	}
+
+	/**
+	 * The preferred way of generating code from a persistent template, this
+	 * stores the generation result in the given file, maintaining any protected
+	 * regions.
+	 * 
+	 * @param path
+	 * @throws EglRuntimeException
+	 */
+	public void generate(String path) throws EglRuntimeException {
+		generate(path, true, true);
+	}
+
+	/**
+	 * Fully parameterised version of generate, usually only used internally.
+	 * Setting overwrite to false implies protectRegions is ignored.
+	 * 
+	 * @param path
+	 * @param overwrite
+	 * @param protectRegions
+	 * @throws EglRuntimeException
+	 */
+	public void generate(String path, boolean overwrite, boolean protectRegions) throws EglRuntimeException {
+		final File outputFile = resolveFile(path);
+
+		if (overwrite || !outputFile.exists()) {
+			if (!isProcessed()) {
+				process();
+			}
+
+			doGenerate(outputFile, name(path), overwrite, protectRegions);
+		} else {
+			addMessage("Existing contents of " + name(path) + " were preserved.");
+		}
+	}
+	
+	private String name(String path) {
+		if (outputRootPath == null) {
+			return path;
+		} else {
+			return new File(outputRootPath).getPath() + FileUtil.FILE_SEP + path;
+		}
+	}
+
+	/**
+	 * Store result of processing template in the given file. Subclasses should
+	 * override to implement the specific semantics of 'storing' to a file.
+	 * The result of processing the template will be available from
+	 * {@link #contents}.
+	 * 
+	 * @param path
+	 * @param targetName
+	 * @param overwrite
+	 * @param protectRegions
+	 */
+	protected abstract void doGenerate(File file, String targetName, boolean overwrite, boolean protectRegions) throws EglRuntimeException;
+
+	
+	protected void addMessage(String message) {
+		callersContext.addStatusMessage(new StatusMessage(message));
+	}
+}
