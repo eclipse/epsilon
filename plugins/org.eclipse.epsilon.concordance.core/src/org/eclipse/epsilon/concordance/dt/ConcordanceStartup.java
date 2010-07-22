86f9ca32c1b43450d3dcce69877c11ba2e7c5002
lrose <lrose@b1e7822e-7b56-0410-88b3-db6ede196368>
2010-07-22 13:52:43 +0000

    Don't display Concordance's EPackageRegistryRefresh task in UI.

--- plugins/org.eclipse.epsilon.concordance.core/src/org/eclipse/epsilon/concordance/dt/ConcordanceStartup.java
+++ plugins/org.eclipse.epsilon.concordance.core/src/org/eclipse/epsilon/concordance/dt/ConcordanceStartup.java
@@ -69,6 +69,7 @@ public class ConcordanceStartup implements IStartup {
 				
 		public PeriodicallyUpdateEPackageRegistryMonitorJob() {
 			super("Update Concordance EPackage Registry Monitor Job");
+			setSystem(true);
 		}
 
 		@Override
