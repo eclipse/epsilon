69f0b80b46875eb4153cf3bb172ac711e2c6294c
dkolovos <dkolovos@b1e7822e-7b56-0410-88b3-db6ede196368>
2010-07-23 19:29:09 +0000

    Added MultilineCommentReader to the SVN

---
+++ tests/org.eclipse.epsilon.test.dependencies/src/org/eclipse/epsilon/test/util/MultilineCommentReader.java
@@ -0,0 +1,96 @@
+package org.eclipse.epsilon.test.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class MultilineCommentReader {
+	
+	protected File findFile(String className, String javafilename) {
+		try {
+			Class clazz = Class.forName(className);
+			String filename = clazz.getResource("").getFile().replace("/bin/", "/src/") + javafilename;
+			return new File(filename);
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+	
+	public String readNextComment() {
+		
+		Exception ex = new Exception();
+		
+		StackTraceElement[] stackTrace = ex.getStackTrace();
+		
+		StackTraceElement s = stackTrace[1];
+		
+		String filename = s.getFileName();
+		
+		File file = findFile(s.getClassName(), filename);
+		
+		String collected = "";
+		
+		if (file != null && file.exists()) {
+			int line = s.getLineNumber();
+			
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader(file));
+				int currentLine = 0;
+				while (reader.readLine() != null && currentLine < line - 1) {
+					currentLine ++;
+				}
+				
+				String leadingWhitespace = "";
+				
+				ReaderState readerState = ReaderState.BeforeComment;
+				while (readerState != ReaderState.AfterComment) {
+					String l = reader.readLine();
+					if (readerState == ReaderState.BeforeComment) {
+						if (l.trim().equals("/*")) readerState = ReaderState.InFirstLineOfComment;
+					}
+					else if (readerState == ReaderState.InComment || readerState == ReaderState.InFirstLineOfComment) {
+						if (l.trim().equals("*/")) {
+							readerState = ReaderState.AfterComment;
+						}
+						else {
+							if (readerState == ReaderState.InFirstLineOfComment) {
+								readerState = ReaderState.InComment;
+								boolean nonWhitespaceFound = false;
+								for (char c : l.toCharArray()) {
+									if (Character.isWhitespace(c)) {
+										if (!nonWhitespaceFound) {
+											leadingWhitespace += c;
+										}
+									}
+									else {
+										nonWhitespaceFound = true;
+									}
+								}
+							}
+							collected = collected + l.replace(leadingWhitespace, "") + "\r\n";
+						}
+					}
+				}
+				
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			return collected.trim();
+			
+		}
+		
+		return null;
+	}
+	
+	enum ReaderState {
+		BeforeComment,
+		InFirstLineOfComment,
+		InComment,
+		AfterComment
+	}
+	
+}
