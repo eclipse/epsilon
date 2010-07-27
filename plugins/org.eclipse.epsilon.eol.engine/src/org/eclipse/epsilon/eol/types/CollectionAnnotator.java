a12bd0d9c0eb8347e5fb5430d30f5eedd252c65c
dkolovos <dkolovos@b1e7822e-7b56-0410-88b3-db6ede196368>
2010-07-27 16:34:59 +0000

    Improved the performance of CollectionAnnotator by adding fastCache

--- plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/types/CollectionAnnotator.java
+++ plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/types/CollectionAnnotator.java
@@ -1,24 +1,57 @@
 package org.eclipse.epsilon.eol.types;
 
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.WeakHashMap;
 
 public class CollectionAnnotator {
 	
 	protected HashMap<WeakReference, AnnotatedCollectionType> cache = new HashMap<WeakReference, AnnotatedCollectionType>();
+	protected HashSet<WeakReference> toRemove = new HashSet<WeakReference>();
+	protected WeakHashMap<Object, Object> fastCache = new WeakHashMap<Object, Object>();
 	
 	protected static CollectionAnnotator instance = new CollectionAnnotator();
 	
 	public void annotate(Collection c, AnnotatedCollectionType ct) {
 		WeakReference ref = new WeakReference(c);
+		fastCache.put(c, null);
 		cache.put(ref, ct);
+		
+		// If the cache grows too much, do a complete
+		// clean up (doesn't help too much)
+//		if (cache.size() > 2 * fastCache.size()) {
+//			for (WeakReference check : cache.keySet()) {
+//				if (check.get() == null) {
+//					toRemove.add(check);
+//				}
+//			}
+//		}
+		
+		if (!toRemove.isEmpty()) {
+			for (WeakReference remove : toRemove) {
+				cache.remove(remove);
+			}
+			toRemove.clear();
+		}
+		
 	}
 	
 	public AnnotatedCollectionType getType(Collection c) {
+		
+		if (!fastCache.containsKey(c)) {
+			return null;
+		}
+		
 		for (WeakReference ref : cache.keySet()) {
-			if (ref.get() == c) {
+			Object target = ref.get();
+			if (target == c) {
 				return cache.get(ref);
+			}
+			else if (target == null) {
+				toRemove.add(ref);
 			}
 		}
 		return null;
