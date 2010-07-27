a0cd0c5e49e173b4f11b52cdd17f64ffc0de0f9d
dkolovos <dkolovos@b1e7822e-7b56-0410-88b3-db6ede196368>
2010-07-27 09:05:00 +0000

    Cleaned up OperationContributorRegistry

--- plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/execute/operations/contributors/OperationContributorRegistry.java
+++ plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/execute/operations/contributors/OperationContributorRegistry.java
@@ -1,12 +1,10 @@
 package org.eclipse.epsilon.eol.execute.operations.contributors;
 
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 import org.eclipse.epsilon.eol.execute.context.IEolContext;
 import org.eclipse.epsilon.eol.execute.introspection.java.ObjectMethod;
 import org.eclipse.epsilon.eol.execute.operations.contributors.compatibility.StringCompatibilityOperationContributor;
-import org.eclipse.epsilon.eol.util.ReflectionUtil;
 
 public class OperationContributorRegistry extends ArrayList<OperationContributor>{
 	
@@ -28,15 +26,6 @@ public class OperationContributorRegistry extends ArrayList<OperationContributor
 			if (c.contributesTo(o)) {
 				ObjectMethod objectMethod = c.getObjectMethodFor(o, name, parameters, context);
 				if (objectMethod != null) return objectMethod;
-//				Method method = ReflectionUtil.getMethodFor(c, name, parameters);
-//				if (method != null) {
-//					objectMethod = new ObjectMethod();
-//					c.setTarget(o);
-//					objectMethod.setMethod(method);
-//					objectMethod.setObject(c);
-//					c.setContext(context);
-//					return objectMethod;
-//				}
 			}
 		}
 		return null;
