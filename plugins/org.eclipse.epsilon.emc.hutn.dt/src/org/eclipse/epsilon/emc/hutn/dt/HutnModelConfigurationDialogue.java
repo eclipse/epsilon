29d952802365bf37b39a7243e27aaa620cb37bbf
lrose <lrose@b1e7822e-7b56-0410-88b3-db6ede196368>
2010-08-26 16:26:49 +0000

    Share project "org.eclipse.epsilon.emc.hutn.dt" into "https://dev.eclipse.org/svnroot/modeling/org.eclipse.gmt.epsilon" Share project "org.eclipse.epsilon.emc.hutn" into "https://dev.eclipse.org/svnroot/modeling/org.eclipse.gmt.epsilon"

---
+++ plugins/org.eclipse.epsilon.emc.hutn.dt/src/org/eclipse/epsilon/emc/hutn/dt/HutnModelConfigurationDialogue.java
@@ -0,0 +1,100 @@
+/*******************************************************************************
+ * Copyright (c) 2010 The University of York.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Louis Rose - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.epsilon.emc.hutn.dt;
+
+import org.eclipse.epsilon.common.dt.launching.dialogs.AbstractModelConfigurationDialog;
+import org.eclipse.epsilon.common.dt.launching.dialogs.BrowseWorkspaceUtil;
+import org.eclipse.epsilon.emc.hutn.HutnModel;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Text;
+
+public class HutnModelConfigurationDialogue extends AbstractModelConfigurationDialog {
+
+	private Text hutnFileText;
+
+	@Override
+	protected String getModelName() {
+		return "EMF model expressed with HUTN.";
+	}
+
+	@Override
+	protected String getModelType() {
+		return "EMF_HUTN";
+	}
+
+	@Override
+	protected void createGroups(Composite parent) {
+		createNameAliasGroup(parent);
+		createLoadStoreOptionsGroup(parent);
+		createHutnFileGroup(parent);
+	}
+	
+	private void createHutnFileGroup(Composite parent) {
+		final Composite groupContent = createGroupContainer(parent, "HUTN Source", 3);
+		
+		final Label hutnFileLabel = new Label(groupContent, SWT.NONE);
+		hutnFileLabel.setText("HUTN file: ");
+		
+		hutnFileText = new Text(groupContent, SWT.BORDER);
+		hutnFileText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		
+		final Button browseHutnFile = new Button(groupContent, SWT.NONE);
+		browseHutnFile.setText("Browse Workspace...");
+		browseHutnFile.addListener(SWT.Selection, new BrowseWorkspaceForHutnSourceFilesListener(hutnFileText));
+		
+		groupContent.layout();
+		groupContent.pack();
+	}
+	
+	@Override
+	protected void loadProperties(){
+		super.loadProperties();
+		
+		if (properties != null) {;
+			hutnFileText.setText(properties.getProperty(HutnModel.PROPERTY_SOURCE_FILE));
+		}
+	}
+	
+	@Override
+	protected void storeProperties(){
+		super.storeProperties();
+		
+		properties.put(HutnModel.PROPERTY_SOURCE_FILE, hutnFileText.getText());
+	}
+	
+	
+	protected class BrowseWorkspaceForHutnSourceFilesListener implements Listener {
+		
+		private final Text text;
+		
+		public BrowseWorkspaceForHutnSourceFilesListener(Text text) {
+			this.text = text;
+		}
+		
+		public void handleEvent(Event event) {
+			final String file = BrowseWorkspaceUtil.browseFilePath(getShell(),
+			                                                       "HUTN source files in the workspace",
+			                                                       "Select a HUTN source file",
+			                                                       "hutn",
+			                                                       null);
+			
+			if (file != null){
+				text.setText(file);
+			}
+		}
+	}
+}
---
+++ plugins/org.eclipse.epsilon.emc.hutn/src/org/eclipse/epsilon/emc/hutn/HutnModel.java
@@ -0,0 +1,221 @@
+/*******************************************************************************
+ * Copyright (c) 2010 The University of York.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Louis Rose - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.epsilon.emc.hutn;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Collection;
+import java.util.Collections;
+
+import org.eclipse.epsilon.commons.parse.problem.ParseProblem;
+import org.eclipse.epsilon.commons.util.FileUtil;
+import org.eclipse.epsilon.commons.util.StringProperties;
+import org.eclipse.epsilon.commons.util.StringUtil;
+import org.eclipse.epsilon.emc.emf.AbstractEmfModel;
+import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
+import org.eclipse.epsilon.eol.exceptions.models.EolEnumerationValueNotFoundException;
+import org.eclipse.epsilon.eol.exceptions.models.EolModelElementTypeNotFoundException;
+import org.eclipse.epsilon.eol.exceptions.models.EolModelLoadingException;
+import org.eclipse.epsilon.eol.exceptions.models.EolNotInstantiableModelElementTypeException;
+import org.eclipse.epsilon.eol.execute.introspection.IPropertyGetter;
+import org.eclipse.epsilon.eol.execute.introspection.IPropertySetter;
+import org.eclipse.epsilon.eol.models.Model;
+import org.eclipse.epsilon.hutn.HutnModule;
+import org.eclipse.epsilon.hutn.IHutnModule;
+import org.eclipse.epsilon.hutn.exceptions.HutnGenerationException;
+import org.eclipse.epsilon.hutn.xmi.HutnXmiBridgeException;
+import org.eclipse.epsilon.hutn.xmi.Xmi2Hutn;
+
+
+public class HutnModel extends Model {
+
+	public static final String PROPERTY_SOURCE_FILE = "sourceFile";
+	
+	private String hutn;
+	private File hutnSourceFile;
+	
+	private AbstractEmfModel model;
+	
+	public HutnModel() {}
+	
+	public HutnModel(String name, String hutn) {
+		this.hutn = hutn;
+		setName(name);
+	}
+
+	public void load() throws EolModelLoadingException {
+		try {
+			final IHutnModule module = new HutnModule();
+			
+			if (hutn != null)
+				module.parse(hutn);
+			else if (hutnSourceFile != null) {
+				module.parse(hutnSourceFile);
+			}
+			
+			if (!module.getParseProblems().isEmpty()) {
+				final StringBuilder problems = new StringBuilder();
+				
+				for (ParseProblem problem : module.getParseProblems()) {
+					problems.append('\n');
+					problems.append(problem);
+				}
+				
+				throw new EolModelLoadingException(new HutnGenerationException("Could not parse HUTN: " + problems), this);
+			}
+
+			model = module.generateEmfModel();
+			
+			model.load();
+		
+		} catch (Exception e) {
+			throw new EolModelLoadingException(e, this);
+		}
+	}
+	
+	@Override
+	public void load(StringProperties properties, String basePath) throws EolModelLoadingException {
+		super.load(properties, basePath);
+		
+		this.hutnSourceFile = new File(StringUtil.toString(basePath) + properties.getProperty(PROPERTY_SOURCE_FILE));
+		
+		load();
+	}
+
+	@Override
+	public IPropertyGetter getPropertyGetter() {
+		return model.getPropertyGetter();
+	}
+
+	@Override
+	public IPropertySetter getPropertySetter() {
+		return model.getPropertySetter();
+	}
+
+	public boolean store(String location) {
+		try {
+			FileUtil.setFileContents(getHutn(), new File(location));
+			return true;
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public boolean store() {
+		return hutnSourceFile == null ? false : store(hutnSourceFile.getAbsolutePath());
+	}
+	
+	@Override
+	public String toString() {
+		try {
+			return getHutn();
+		
+		} catch (HutnXmiBridgeException e) {
+			return "HutnModel(Could not deseralise to HUTN source)"; 
+		}
+	}
+
+	private String getHutn() throws HutnXmiBridgeException {
+		try {
+			final StringOutputStream xmi = new StringOutputStream();
+			
+			model.getModelImpl().save(xmi, Collections.EMPTY_MAP);
+			
+			return new Xmi2Hutn(xmi.toString()).getHutn();
+		
+		} catch (IOException e) { // Using a StringOutputStream
+			e.printStackTrace();  // so this should never happen
+			return "";
+		}
+	}
+	
+	class StringOutputStream extends OutputStream {
+		
+		StringBuffer buffer = new StringBuffer();
+		
+		@Override
+		public void write(int chr) throws IOException {
+			buffer.append((char) chr);
+		}
+		
+		@Override
+		public String toString() {
+			return buffer.toString();
+		}
+	}
+	
+	
+// Delegates //	
+
+	public Object getEnumerationValue(String enumeration, String label) throws EolEnumerationValueNotFoundException {
+		return model.getEnumerationValue(enumeration, label);
+	}
+
+	public Collection<?> allContents() {
+		return model.allContents();
+	}
+
+	public Collection<?> getAllOfType(String type) throws EolModelElementTypeNotFoundException {
+		return model.getAllOfType(type);
+	}
+
+	public Collection<?> getAllOfKind(String type) throws EolModelElementTypeNotFoundException {
+		return model.getAllOfKind(type);
+	}
+
+	public Object getTypeOf(Object instance) {
+		return model.getTypeOf(instance);
+	}
+
+	public String getTypeNameOf(Object instance) {
+		return model.getTypeNameOf(instance);
+	}
+
+	public Object createInstance(String type) throws EolModelElementTypeNotFoundException, EolNotInstantiableModelElementTypeException {
+		return model.createInstance(type);
+	}
+
+	public Object getElementById(String id) {
+		return model.getElementById(id);
+	}
+
+	public String getElementId(Object instance) {
+		return model.getElementId(instance);
+	}
+
+	public void setElementId(Object instance, String newId) {
+		model.setElementId(instance, newId);
+	}
+
+	public void deleteElement(Object instance) throws EolRuntimeException {
+		model.deleteElement(instance);
+	}
+
+	public boolean owns(Object instance) {
+		return model.owns(instance);
+	}
+
+	public boolean isInstantiable(String type) {
+		return model.isInstantiable(type);
+	}
+	
+	public boolean isModelElement(Object instance) {
+		return isModelElement(instance);
+	}
+
+	public boolean hasType(String type) {
+		return model.hasType(type);
+	}
+}
