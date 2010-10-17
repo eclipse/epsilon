609a7926cccd9c8f00158953f26c14f230d5a3a2
dkolovos <dkolovos@b1e7822e-7b56-0410-88b3-db6ede196368>
2010-10-17 09:54:46 +0000

    Fixes bug where return statements are ignored in switch/case/default

--- plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/execute/SwitchStatementExecutor.java
+++ plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/execute/SwitchStatementExecutor.java
@@ -32,8 +32,9 @@ public class SwitchStatementExecutor extends AbstractExecutor {
 						AST caseBodyStatement = caseValueAst.getNextSibling();
 						try {
 							while (caseBodyStatement != null) {
-								context.getExecutorFactory().executeAST(caseBodyStatement, context);
-								caseBodyStatement = caseBodyStatement.getNextSibling();
+								Object result = context.getExecutorFactory().executeAST(caseBodyStatement, context);
+								if (result instanceof Return) return result;
+ 								caseBodyStatement = caseBodyStatement.getNextSibling();
 							}
 						}
 						catch (EolContinueException ex) {
@@ -47,7 +48,8 @@ public class SwitchStatementExecutor extends AbstractExecutor {
 				else if (nextCase.getType() == EolParser.DEFAULT) {
 					AST defaultBodyStatement = nextCase.getFirstChild();
 					while (defaultBodyStatement != null) {
-						context.getExecutorFactory().executeAST(defaultBodyStatement, context);
+						Object result = context.getExecutorFactory().executeAST(defaultBodyStatement, context);
+						if (result instanceof Return) return result;
 						defaultBodyStatement = defaultBodyStatement.getNextSibling();
 					}
 				}
