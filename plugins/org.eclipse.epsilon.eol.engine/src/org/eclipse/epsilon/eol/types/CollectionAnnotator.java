5f0d92054ffcfb9c76090fa31f75ba03feaef5a0
dkolovos <dkolovos@b1e7822e-7b56-0410-88b3-db6ede196368>
2011-02-24 14:12:13 +0000

    Fixes bug #338087

--- plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/types/CollectionAnnotator.java
+++ plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/types/CollectionAnnotator.java
@@ -1,24 +1,21 @@
 package org.eclipse.epsilon.eol.types;
 
-import java.lang.ref.WeakReference;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Map;
 import java.util.WeakHashMap;
 
 public class CollectionAnnotator {
 	
-	protected HashMap<WeakReference, AnnotatedCollectionType> cache = new HashMap<WeakReference, AnnotatedCollectionType>();
-	protected HashSet<WeakReference> toRemove = new HashSet<WeakReference>();
-	protected WeakHashMap<Object, Object> fastCache = new WeakHashMap<Object, Object>();
+	protected Map<Object, AnnotatedCollectionType> cache = new WeakHashMap<Object, AnnotatedCollectionType>();
+	//protected HashSet<WeakReference> toRemove = new HashSet<WeakReference>();
+	//protected WeakHashMap<Object, Object> fastCache = new WeakHashMap<Object, Object>();
 	
 	protected static CollectionAnnotator instance = new CollectionAnnotator();
 	
 	public void annotate(Collection c, AnnotatedCollectionType ct) {
-		WeakReference ref = new WeakReference(c);
-		fastCache.put(c, null);
-		cache.put(ref, ct);
+		//WeakReference ref = new WeakReference(c);
+		//fastCache.put(c, null);
+		cache.put(c, ct);
 		
 		// If the cache grows too much, do a complete
 		// clean up (doesn't help too much)
@@ -30,31 +27,34 @@ public class CollectionAnnotator {
 //			}
 //		}
 		
-		if (!toRemove.isEmpty()) {
-			for (WeakReference remove : toRemove) {
-				cache.remove(remove);
-			}
-			toRemove.clear();
-		}
+		//if (!toRemove.isEmpty()) {
+		//	for (WeakReference remove : toRemove) {
+		//		cache.remove(remove);
+		//	}
+		//	toRemove.clear();
+		//}
 		
 	}
 	
 	public AnnotatedCollectionType getType(Collection c) {
 		
-		if (!fastCache.containsKey(c)) {
-			return null;
-		}
+//		if (!fastCache.containsKey(c)) {
+//			return null;
+//		}
+//		
+//		for (WeakReference ref : cache.keySet()) {
+//			Object target = ref.get();
+//			if (target == c) {
+//				return cache.get(ref);
+//			}
+//			else if (target == null) {
+//				toRemove.add(ref);
+//			}
+//		}
+//		return null;
 		
-		for (WeakReference ref : cache.keySet()) {
-			Object target = ref.get();
-			if (target == c) {
-				return cache.get(ref);
-			}
-			else if (target == null) {
-				toRemove.add(ref);
-			}
-		}
-		return null;
+		return cache.get(c);
+		
 	}
 	
 	public static CollectionAnnotator getInstance() {
