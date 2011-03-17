5dbdb7fdac057be2087db18059dfadc991e3bd52
dkolovos <dkolovos@b1e7822e-7b56-0410-88b3-db6ede196368>
2011-03-17 14:45:34 +0000

    Fixed memory leak

--- plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/types/CollectionAnnotator.java
+++ plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/types/CollectionAnnotator.java
@@ -1,60 +1,22 @@
 package org.eclipse.epsilon.eol.types;
 
+import java.lang.ref.WeakReference;
 import java.util.Collection;
 import java.util.Map;
 import java.util.WeakHashMap;
 
 public class CollectionAnnotator {
 	
-	protected Map<Object, AnnotatedCollectionType> cache = new WeakHashMap<Object, AnnotatedCollectionType>();
-	//protected HashSet<WeakReference> toRemove = new HashSet<WeakReference>();
-	//protected WeakHashMap<Object, Object> fastCache = new WeakHashMap<Object, Object>();
+	protected Map<IdentityBasedWeakReference, AnnotatedCollectionType> cache = new WeakHashMap<IdentityBasedWeakReference, AnnotatedCollectionType>();
 	
 	protected static CollectionAnnotator instance = new CollectionAnnotator();
 	
 	public void annotate(Collection c, AnnotatedCollectionType ct) {
-		//WeakReference ref = new WeakReference(c);
-		//fastCache.put(c, null);
-		cache.put(c, ct);
-		
-		// If the cache grows too much, do a complete
-		// clean up (doesn't help too much)
-//		if (cache.size() > 2 * fastCache.size()) {
-//			for (WeakReference check : cache.keySet()) {
-//				if (check.get() == null) {
-//					toRemove.add(check);
-//				}
-//			}
-//		}
-		
-		//if (!toRemove.isEmpty()) {
-		//	for (WeakReference remove : toRemove) {
-		//		cache.remove(remove);
-		//	}
-		//	toRemove.clear();
-		//}
-		
+		cache.put(new IdentityBasedWeakReference(c), ct);
 	}
 	
-	public AnnotatedCollectionType getType(Collection c) {
-		
-//		if (!fastCache.containsKey(c)) {
-//			return null;
-//		}
-//		
-//		for (WeakReference ref : cache.keySet()) {
-//			Object target = ref.get();
-//			if (target == c) {
-//				return cache.get(ref);
-//			}
-//			else if (target == null) {
-//				toRemove.add(ref);
-//			}
-//		}
-//		return null;
-		
-		return cache.get(c);
-		
+	public AnnotatedCollectionType getType(Collection c) {		
+		return cache.get(new IdentityBasedWeakReference(c));
 	}
 	
 	public static CollectionAnnotator getInstance() {
@@ -68,4 +30,27 @@ public class CollectionAnnotator {
 		OrderedSet
 	}
 	
+	class IdentityBasedWeakReference extends WeakReference<Object> {
+
+		public IdentityBasedWeakReference(Object referent) {
+			super(referent);
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if (obj == null) return false;
+			else return this.hashCode() == obj.hashCode();
+		}
+		
+		@Override
+		public int hashCode() {
+			if (this.get() != null) {
+				return System.identityHashCode(this.get());
+			}
+			else {
+				return super.hashCode();
+			}
+		}
+		
+	}
 }
