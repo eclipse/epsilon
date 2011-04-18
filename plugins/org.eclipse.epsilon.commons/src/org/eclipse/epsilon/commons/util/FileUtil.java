1ed22e01a93528f84ff0eb7622b0962f28155115
agarciad <agarciad@b1e7822e-7b56-0410-88b3-db6ede196368>
2011-04-18 15:59:12 +0000

    TreeEqualityAssertionOperation: move file-related methods to FileUtil

--- plugins/org.eclipse.epsilon.commons/src/org/eclipse/epsilon/commons/util/FileUtil.java
+++ plugins/org.eclipse.epsilon.commons/src/org/eclipse/epsilon/commons/util/FileUtil.java
@@ -12,10 +12,19 @@ package org.eclipse.epsilon.commons.util;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.nio.channels.FileChannel;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FileUtil {
 	
@@ -72,5 +81,120 @@ public class FileUtil {
 	
 	public static String getPath(String name, Class<?> relativeTo) {
 		return getFile(name, relativeTo).getAbsolutePath();
+	}
+
+	public static void checkFileExists(final File file) throws FileNotFoundException {
+		if (!file.exists()) {
+			throw new FileNotFoundException(
+				"File " + file.getPath() + " does not exist");
+		}
+	}
+
+	public static File copyToTemp(File srcFile) throws IOException {
+		File tmpFile = File.createTempFile("filecompare", "tmp");
+		if (srcFile.isDirectory()) {
+			tmpFile.delete();
+			tmpFile.mkdir();
+		}
+		copy(srcFile, tmpFile);
+		return tmpFile;
+	}
+
+	public static void copy(File srcFile, File dstFile) throws IOException {
+		if (srcFile.isDirectory()) {
+			dstFile.mkdir();
+			for (File entry : srcFile.listFiles()) {
+				copy(entry, new File(dstFile, entry.getName()));
+			}
+		}
+		else {
+			// Based on the second answer in http://stackoverflow.com/questions/106770.
+			FileInputStream isSrc = null;
+			FileOutputStream osDst = null;
+			FileChannel chSrc = null;
+			FileChannel chDst = null;
+			try {
+				isSrc = new FileInputStream(srcFile);
+				osDst = new FileOutputStream(dstFile);
+				chSrc = isSrc.getChannel();
+				chDst = osDst.getChannel();
+				final long srcBytes = srcFile.length();
+				long transferred = 0;
+				while (transferred < srcBytes) {
+					transferred += chDst.transferFrom(chSrc, transferred, srcBytes);
+					chDst.position(transferred);
+				}
+			}
+			finally {
+				if (chDst != null) {
+					chDst.close();
+				}
+				else if (osDst != null) {
+					osDst.close();
+				}
+
+				if (chSrc != null) {
+					chSrc.close();
+				}
+				else if (isSrc != null) {
+					isSrc.close();
+				}
+			}
+		}
+	}
+
+	public static HashSet<String> listFilesAsSet(File fileExpected) {
+		return new HashSet<String>(Arrays.asList(fileExpected.list()));
+	}
+
+	/**
+	 * We implement our own comparison algorithm here, so we don't need Eclipse
+	 * Compare to compute differences, but rather only to show them in the UI.
+	 */
+	public static boolean sameContents(File fileExpected, File fileActual) throws IOException {
+		if (fileExpected.isDirectory() != fileActual.isDirectory()) {
+			// One is a file, the other is a directory: not the same
+			return false;
+		}
+
+		if (fileExpected.isDirectory()) {
+			// Both are directories: they should contain the same filenames,
+			// and each pair should have the same contents
+			final Set<String> expectedFilenames = listFilesAsSet(fileExpected);
+			final Set<String> actualFilenames = listFilesAsSet(fileActual);
+			if (!expectedFilenames.equals(actualFilenames)) {
+				return false;
+			}
+			for (String filename : expectedFilenames) {
+				final File expectedEntry = new File(fileExpected, filename);
+				final File actualEntry = new File(fileActual, filename);
+				if (!sameContents(expectedEntry, actualEntry)) {
+					return false;
+				}
+			}
+			return true;
+		}
+		else {
+			if (fileExpected.length() != fileActual.length()) {
+				// Different length: no need to read the files
+				return false;
+			}
+
+			final FileInputStream isExpected = new FileInputStream(fileExpected);
+			final FileInputStream isActual   = new FileInputStream(fileActual);
+			return sameContents(isExpected, isActual);
+		}
+	}
+
+	public static boolean sameContents(InputStream isExpected, InputStream isActual) throws IOException {
+		int chExpected, chActual;
+	
+		do {
+			chExpected = isExpected.read();
+			chActual = isActual.read();
+		}
+		while (chExpected == chActual && chExpected > 0 && chActual > 0);
+	
+		return chExpected == chActual;
 	}
 }
--- plugins/org.eclipse.epsilon.eunit.engine/src/org/eclipse/epsilon/eunit/execute/operations/TreeEqualityAssertionOperation.java
+++ plugins/org.eclipse.epsilon.eunit.engine/src/org/eclipse/epsilon/eunit/execute/operations/TreeEqualityAssertionOperation.java
@@ -1,18 +1,10 @@
 package org.eclipse.epsilon.eunit.execute.operations;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.channels.FileChannel;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.eclipse.epsilon.commons.parse.AST;
+import org.eclipse.epsilon.commons.util.FileUtil;
 import org.eclipse.epsilon.eol.exceptions.EolAssertionException;
 import org.eclipse.epsilon.eol.exceptions.EolInternalException;
 import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
@@ -45,9 +37,9 @@ public class TreeEqualityAssertionOperation extends AbstractSimpleOperation {
 		File fileActual = new File(pathActual);
 		try {
 			// Compare and check against results
-			checkFileExists(fileExpected);
-			checkFileExists(fileActual);
-			if (sameContents(fileExpected, fileActual) == mustBeEqual) {
+			FileUtil.checkFileExists(fileExpected);
+			FileUtil.checkFileExists(fileActual);
+			if (FileUtil.sameContents(fileExpected, fileActual) == mustBeEqual) {
 				return true;
 			}
 
@@ -56,8 +48,8 @@ public class TreeEqualityAssertionOperation extends AbstractSimpleOperation {
 				// the user may want to see the differences later. We need
 				// to copy the files to a temporary location so they can be
 				// safely compared later on.
-				fileExpected = copyToTemp(fileExpected);
-				fileActual   = copyToTemp(fileActual);
+				fileExpected = FileUtil.copyToTemp(fileExpected);
+				fileActual   = FileUtil.copyToTemp(fileActual);
 			}
 			else {
 				// There are no differences: disable the 'Compare' button
@@ -76,120 +68,5 @@ public class TreeEqualityAssertionOperation extends AbstractSimpleOperation {
 		// in showing differences in the UI.
 		throw new EolAssertionException(
 			message, ast, fileExpected, fileActual, null);
-	}
-
-	private static void checkFileExists(final File file) throws FileNotFoundException {
-		if (!file.exists()) {
-			throw new FileNotFoundException(
-				"File " + file.getPath() + " does not exist");
-		}
-	}
-
-	private static File copyToTemp(File srcFile) throws IOException {
-		File tmpFile = File.createTempFile("filecompare", "tmp");
-		if (srcFile.isDirectory()) {
-			tmpFile.delete();
-			tmpFile.mkdir();
-		}
-		copy(srcFile, tmpFile);
-		return tmpFile;
-	}
-
-	private static void copy(File srcFile, File dstFile) throws IOException {
-		if (srcFile.isDirectory()) {
-			dstFile.mkdir();
-			for (File entry : srcFile.listFiles()) {
-				copy(entry, new File(dstFile, entry.getName()));
-			}
-		}
-		else {
-			// Based on the second answer in http://stackoverflow.com/questions/106770.
-			FileInputStream isSrc = null;
-			FileOutputStream osDst = null;
-			FileChannel chSrc = null;
-			FileChannel chDst = null;
-			try {
-				isSrc = new FileInputStream(srcFile);
-				osDst = new FileOutputStream(dstFile);
-				chSrc = isSrc.getChannel();
-				chDst = osDst.getChannel();
-				final long srcBytes = srcFile.length();
-				long transferred = 0;
-				while (transferred < srcBytes) {
-					transferred += chDst.transferFrom(chSrc, transferred, srcBytes);
-					chDst.position(transferred);
-				}
-			}
-			finally {
-				if (chDst != null) {
-					chDst.close();
-				}
-				else if (osDst != null) {
-					osDst.close();
-				}
-
-				if (chSrc != null) {
-					chSrc.close();
-				}
-				else if (isSrc != null) {
-					isSrc.close();
-				}
-			}
-		}
-	}
-
-	private static HashSet<String> listFilesAsSet(File fileExpected) {
-		return new HashSet<String>(Arrays.asList(fileExpected.list()));
-	}
-
-	/**
-	 * We implement our own comparison algorithm here, so we don't need Eclipse
-	 * Compare to compute differences, but rather only to show them in the UI.
-	 */
-	private static boolean sameContents(File fileExpected, File fileActual) throws IOException {
-		if (fileExpected.isDirectory() != fileActual.isDirectory()) {
-			// One is a file, the other is a directory: not the same
-			return false;
-		}
-
-		if (fileExpected.isDirectory()) {
-			// Both are directories: they should contain the same filenames,
-			// and each pair should have the same contents
-			final Set<String> expectedFilenames = listFilesAsSet(fileExpected);
-			final Set<String> actualFilenames = listFilesAsSet(fileActual);
-			if (!expectedFilenames.equals(actualFilenames)) {
-				return false;
-			}
-			for (String filename : expectedFilenames) {
-				final File expectedEntry = new File(fileExpected, filename);
-				final File actualEntry = new File(fileActual, filename);
-				if (!sameContents(expectedEntry, actualEntry)) {
-					return false;
-				}
-			}
-			return true;
-		}
-		else {
-			if (fileExpected.length() != fileActual.length()) {
-				// Different length: no need to read the files
-				return false;
-			}
-
-			final FileInputStream isExpected = new FileInputStream(fileExpected);
-			final FileInputStream isActual   = new FileInputStream(fileActual);
-			return sameContents(isExpected, isActual);
-		}
-	}
-
-	private static boolean sameContents(InputStream isExpected, InputStream isActual) throws IOException {
-		int chExpected, chActual;
-
-		do {
-			chExpected = isExpected.read();
-			chActual = isActual.read();
-		}
-		while (chExpected == chActual && chExpected > 0 && chActual > 0);
-
-		return chExpected == chActual;
 	}
 }
