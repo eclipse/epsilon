94a24e5a92ee05a30caf0c1fb53a0750b25a0a0f
agarciad <agarciad@b1e7822e-7b56-0410-88b3-db6ede196368>
2011-04-18 15:57:30 +0000

    eunit.engine: first version of assertEqualFiles
    This is the first version of the file comparison assertion, using the
    Eclipse differencer in the Compare component. However, for some reason,
    I have to bring SWT just because ITypedElement has Image as the return
    type of getImage(). Once I get the first version done, I need to think
    of a way to perform file/directory comparison without depending on the
    UI. Ideally, it should work like for strings: no UI needed for comparing
    them, only for showing the differences.

---
+++ plugins/org.eclipse.epsilon.eunit.engine/src/org/eclipse/epsilon/eunit/execute/operations/FilesystemTreeNode.java
@@ -0,0 +1,89 @@
+package org.eclipse.epsilon.eunit.execute.operations;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.compare.IModificationDate;
+import org.eclipse.compare.IStreamContentAccessor;
+import org.eclipse.compare.ITypedElement;
+import org.eclipse.compare.structuremergeviewer.IStructureComparator;
+import org.eclipse.swt.graphics.Image;
+
+public class FilesystemTreeNode
+	implements IStructureComparator, IModificationDate, ITypedElement, IStreamContentAccessor
+{
+	private final File file;
+
+	public FilesystemTreeNode(File file) {
+		this.file = file;
+	}
+
+	@Override
+	public String getName() {
+		return file.getName();
+	}
+
+	@Override
+	public String getType() {
+		if (file.isDirectory()) {
+			return ITypedElement.FOLDER_TYPE;
+		}
+		else {
+			final String extension = getExtension();
+			if (extension != null) {
+				return extension;
+			}
+			else {
+				return ITypedElement.UNKNOWN_TYPE;
+			}
+		}
+	}
+
+	private String getExtension() {
+		final String name = file.getName();
+		final int lastDot = name.lastIndexOf('.');
+		String extension = null;
+		if (lastDot != -1) {
+			extension = name.substring(lastDot + 1);
+		}
+		return extension;
+	}
+
+	@Override
+	public long getModificationDate() {
+		return file.lastModified();
+	}
+
+	@Override
+	public Object[] getChildren() {
+		File[] files = file.listFiles();
+		if (files == null) return files;
+
+		List<FilesystemTreeNode> children = new ArrayList<FilesystemTreeNode>();
+		for (File childFile : file.listFiles()) {
+			children.add(new FilesystemTreeNode(childFile));
+		}
+		return children.toArray();
+	}
+
+	@Override
+	public Image getImage() {
+		return null;
+	}
+
+	@Override
+	public InputStream getContents() {
+		try {
+			return new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			// FIXME use proper logging here, after moving to eunit.dt
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+}
--- plugins/org.eclipse.epsilon.eunit.engine/src/org/eclipse/epsilon/eunit/execute/operations/TreeEqualityAssertionOperation.java
+++ plugins/org.eclipse.epsilon.eunit.engine/src/org/eclipse/epsilon/eunit/execute/operations/TreeEqualityAssertionOperation.java
@@ -1,24 +1,67 @@
 package org.eclipse.epsilon.eunit.execute.operations;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
 
+import org.eclipse.compare.structuremergeviewer.Differencer;
 import org.eclipse.epsilon.commons.parse.AST;
+import org.eclipse.epsilon.eol.exceptions.EolAssertionException;
+import org.eclipse.epsilon.eol.exceptions.EolInternalException;
 import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
 import org.eclipse.epsilon.eol.execute.context.IEolContext;
 import org.eclipse.epsilon.eol.execute.operations.simple.AbstractSimpleOperation;
 
 public class TreeEqualityAssertionOperation extends AbstractSimpleOperation {
 
-	public TreeEqualityAssertionOperation(boolean b) {
-		// TODO Auto-generated constructor stub
+	private final boolean mustBeEqual;
+
+	public TreeEqualityAssertionOperation(boolean mustBeEqual) {
+		this.mustBeEqual = mustBeEqual;
 	}
 
 	@SuppressWarnings("rawtypes")
 	@Override
-	public Object execute(Object source, List parameters, IEolContext context,
-			AST ast) throws EolRuntimeException {
-		// TODO Auto-generated method stub
-		return null;
+	public Object execute(
+			Object source, List parameters, IEolContext context, AST ast)
+			throws EolRuntimeException
+	{
+		if (parameters.size() != 2) {
+			throw new EolRuntimeException(
+				"Expected 2 paths, but got " + parameters.size() + " arguments");
+		}
+
+		// Make sure both files exist
+		final String pathExpected = (String)parameters.remove(0);
+		final String pathActual = (String)parameters.remove(0);
+		final File fileExpected = new File(pathExpected);
+		final File fileActual = new File(pathActual);
+		try {
+			if (!fileExpected.exists()) {
+				throw new FileNotFoundException(fileExpected.getPath() + " does not exist");
+			}
+			else if (!fileActual.exists()) {
+				throw new FileNotFoundException(fileActual.getPath() + " does not exist");
+			}
+		} catch (FileNotFoundException ex) {
+			throw new EolInternalException(ex);
+		}
+
+		// Compare and check against results
+		final FilesystemTreeNode expected = new FilesystemTreeNode(fileExpected);
+		final FilesystemTreeNode actual = new FilesystemTreeNode(fileActual);
+		final Differencer diffEngine = new Differencer();
+		final Object delta
+			= diffEngine.findDifferences(false, null, null, null, expected, actual);
+		if ((delta == null) == mustBeEqual) {
+			return true;
+		}
+
+		String message = String.format(
+			"Expected %s and %s to be %s, but they weren't",
+			pathExpected, pathActual, mustBeEqual ? "equal" : "different");
+		throw new EolAssertionException(
+			message, ast, fileExpected, fileActual, delta);
 	}
 
 }
