100a7c4d74e7f8d6fb12bb3b34cb31cf016f61f6
agarciad <agarciad@b1e7822e-7b56-0410-88b3-db6ede196368>
2011-05-02 13:13:05 +0000

    [Eugenia] Add setLastStep() to control when to stop

--- plugins/org.eclipse.epsilon.eugenia/src/org/eclipse/epsilon/eugenia/GenerateAllDelegate.java
+++ plugins/org.eclipse.epsilon.eugenia/src/org/eclipse/epsilon/eugenia/GenerateAllDelegate.java
@@ -25,7 +25,8 @@ public class GenerateAllDelegate implements IObjectActionDelegate {
 	private GmfFileSet gmfFileSet;
 	private Shell shell;
 	private IWorkbenchPart targetPart;
-	
+	private GenerateAllStep lastStep = GenerateAllStep.gmfcode;
+
 	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
 		this.shell = targetPart.getSite().getShell();
 		this.targetPart = targetPart;
@@ -66,7 +67,8 @@ public class GenerateAllDelegate implements IObjectActionDelegate {
 		ClearGmfFileSetAction clearGmfFileSetAction = new ClearGmfFileSetAction();
 		clearGmfFileSetAction.setSelectedFile(selectedFile);
 		clearGmfFileSetAction.runImpl(action);
-		
+		if (lastStep.compareTo(GenerateAllStep.clean) <= 0) return;
+
 		if (getSelectedFile().getLocationURI().toString().equals(gmfFileSet.getEmfaticPath())) {
 			// Do Emfatic to Ecore transformation
 			Emfatic2EcoreDelegate emfatic2EcoreDelegate = new Emfatic2EcoreDelegate();
@@ -74,15 +76,19 @@ public class GenerateAllDelegate implements IObjectActionDelegate {
 			emfatic2EcoreDelegate.runImpl(action);
 			emfatic2EcoreDelegate.refresh();
 		}
-		
+		// Using compareTo instead of == is a bit more error-proof if we add new steps
+		// or rearrange them.
+		if (lastStep.compareTo(GenerateAllStep.ecore) <= 0) return;
+
 		// Do Ecore to GenModel transformation
 		Ecore2GenModelDelegate ecore2GenModelDelegate = new Ecore2GenModelDelegate();
 		ecore2GenModelDelegate.setClearConsole(false);
 		ecore2GenModelDelegate.setSelectedFile(selectedFile);
 		ecore2GenModelDelegate.runImpl(action);
 		ecore2GenModelDelegate.refresh();
-		
+
 		WorkspaceUtil.waitFor(gmfFileSet.getGenModelPath());
+		if (lastStep.compareTo(GenerateAllStep.genmodel) <= 0) return;
 		
 		// Do Ecore  to GmfTool, GmfGraph and GmfMap
 		GenerateToolGraphMapDelegate generateToolGraphMapDelegate = new GenerateToolGraphMapDelegate();
@@ -90,7 +96,7 @@ public class GenerateAllDelegate implements IObjectActionDelegate {
 		generateToolGraphMapDelegate.setSelectedFile(selectedFile);
 		generateToolGraphMapDelegate.run(action);
 		generateToolGraphMapDelegate.refresh();
-		
+
 		WorkspaceUtil.waitFor(gmfFileSet.getGmfMapPath());
 		
 		// Do GmfMap to GmfGen
@@ -107,7 +113,8 @@ public class GenerateAllDelegate implements IObjectActionDelegate {
 		fixGmfGenDelegate.setClearConsole(false);
 		fixGmfGenDelegate.setSelectedFile(selectedFile);
 		fixGmfGenDelegate.run(action);
-		
+		if (lastStep.compareTo(GenerateAllStep.gmf) <= 0) return;
+
 		// Generate code from EMF
 		GenerateEmfCodeDelegate generateEmfCodeDelegate = new GenerateEmfCodeDelegate();
 		generateEmfCodeDelegate.setSelectedFile(selectedFile);
@@ -119,7 +126,8 @@ public class GenerateAllDelegate implements IObjectActionDelegate {
 		}
 		
 		getSelectedFile().refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
-		
+		if (lastStep.compareTo(GenerateAllStep.emfcode) <= 0) return;
+
 		// Generate diagram code from GmfGen
 		final GenerateDiagramCodeDelegate generateDiagramCodeDelegate = new GenerateDiagramCodeDelegate();
 		generateDiagramCodeDelegate.setSelectedFile(selectedFile);
@@ -145,6 +153,14 @@ public class GenerateAllDelegate implements IObjectActionDelegate {
 	public void setSelectedFile(IFile file) {
 		selectedFile = file;
 		gmfFileSet = new GmfFileSet(getSelectedFile().getLocationURI().toString());
+	}
+
+	public void setLastStep(GenerateAllStep lastStep) {
+		this.lastStep = lastStep;
+	}
+
+	public GenerateAllStep getLastStep() {
+		return lastStep;
 	}
 
 	public void selectionChanged(IAction action, ISelection sel) {
---
+++ plugins/org.eclipse.epsilon.eugenia/src/org/eclipse/epsilon/eugenia/GenerateAllStep.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2011 The University of York.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Antonio Garcia-Dominguez - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.epsilon.eugenia;
+
+/**
+ * Names of the different steps in the Eugenia workflow. Useful in
+ * {@link GenerateAllDelegate#setLastStep(GenerateAllStep)}.
+ * 
+ * Please note that these enum values must be kept in the same order
+ * as the steps in Eugenia. We use lowercase names instead of the
+ * usual uppercase names so the Ant tasks look nicer.
+ */
+public enum GenerateAllStep {
+	clean,
+	ecore,
+	genmodel,
+	gmf,
+	emfcode,
+	gmfcode
+}
