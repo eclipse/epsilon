5103d83d87ae9e1d03d99530938a970a19396642
agarciad <agarciad@b1e7822e-7b56-0410-88b3-db6ede196368>
2011-05-17 11:29:50 +0000

    Fix bug #346016

--- plugins/org.eclipse.epsilon.common.dt/src/org/eclipse/epsilon/common/dt/editor/AbstractModuleEditorPartitionScanner.java
+++ plugins/org.eclipse.epsilon.common.dt/src/org/eclipse/epsilon/common/dt/editor/AbstractModuleEditorPartitionScanner.java
@@ -23,10 +23,10 @@ public class AbstractModuleEditorPartitionScanner extends RuleBasedPartitionScan
 	public AbstractModuleEditorPartitionScanner() {
 		final IToken comment = new Token(COMMENT);
 
-		IPredicateRule[] rules = new IPredicateRule[2];
-		
-		rules[0] = new MultiLineRule("-*", "*-", comment, (char) 0, true);
-		rules[1] = new MultiLineRule("/*", "*/", comment, (char) 0, true);
+		IPredicateRule[] rules = new IPredicateRule[] {
+				new MultiLineRule("-*", "*-", comment, (char) 0, false),
+				new MultiLineRule("/*", "*/", comment, (char) 0, false),
+		};
 
 		setPredicateRules(rules);
 	}
