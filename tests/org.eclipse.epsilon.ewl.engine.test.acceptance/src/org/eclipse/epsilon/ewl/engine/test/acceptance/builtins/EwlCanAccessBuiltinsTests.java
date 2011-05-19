f2d2bf7a1aa5b2fd17f39e0ee41b5f73e139471a
lrose <lrose@b1e7822e-7b56-0410-88b3-db6ede196368>
2011-05-19 16:11:44 +0000

    Share project "org.eclipse.epsilon.ewl.engine.test.acceptance" into "https://dev.eclipse.org/svnroot/modeling/org.eclipse.gmt.epsilon"

---
+++ tests/org.eclipse.epsilon.ewl.engine.test.acceptance/src/org/eclipse/epsilon/ewl/engine/test/acceptance/builtins/EwlCanAccessBuiltinsTests.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2011 The University of York.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Louis Rose - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.epsilon.ewl.engine.test.acceptance.builtins;
+
+
+import java.io.File;
+import java.util.List;
+
+import org.eclipse.epsilon.commons.util.FileUtil;
+import org.eclipse.epsilon.eol.IEolLibraryModule;
+import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
+import org.eclipse.epsilon.ewl.EwlModule;
+import org.eclipse.epsilon.ewl.EwlWizardInstance;
+import org.eclipse.epsilon.test.builtins.CanAccessBuiltinsTests;
+
+public class EwlCanAccessBuiltinsTests extends CanAccessBuiltinsTests {
+
+	@Override
+	protected IEolLibraryModule createModule() {
+		return new EwlModule();
+	}
+
+	@Override
+	protected File getProgram() {
+		return FileUtil.getFile(("System.ewl"), EwlCanAccessBuiltinsTests.class);
+	}
+
+	@Override
+	protected String getExpectedPrintedValue() {
+		return "Running EWL";
+	}
+	
+	@Override
+	protected void execute(IEolLibraryModule module) throws EolRuntimeException {
+		final List<EwlWizardInstance> wizards = ((EwlModule)module).getWizardsFor("Running EWL");
+		
+		for (EwlWizardInstance wizard : wizards) {
+			wizard.process();
+		}
+	}
+}
