5173ee022f6b41a5ec6778ca53c1a96d51b46be5
lrose <lrose@b1e7822e-7b56-0410-88b3-db6ede196368>
2011-05-19 16:06:36 +0000

    Share project "org.eclipse.epsilon.ecl.engine.test.acceptance" into "https://dev.eclipse.org/svnroot/modeling/org.eclipse.gmt.epsilon"

---
+++ tests/org.eclipse.epsilon.ecl.engine.test.acceptance/src/org/eclipse/epsilon/ecl/engine/test/acceptance/EclAcceptanceTestSuite.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2011 The University of York.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Louis Rose - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.epsilon.ecl.engine.test.acceptance;
+
+import org.eclipse.epsilon.ecl.engine.test.acceptance.builtins.EclCanAccessBuiltinsTests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({EclCanAccessBuiltinsTests.class})
+public class EclAcceptanceTestSuite {
+
+}
---
+++ tests/org.eclipse.epsilon.ecl.engine.test.acceptance/src/org/eclipse/epsilon/ecl/engine/test/acceptance/builtins/EclCanAccessBuiltinsTests.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2011 The University of York.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Louis Rose - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.epsilon.ecl.engine.test.acceptance.builtins;
+
+import java.io.File;
+
+import org.eclipse.epsilon.commons.util.FileUtil;
+import org.eclipse.epsilon.ecl.EclModule;
+import org.eclipse.epsilon.eol.IEolLibraryModule;
+import org.eclipse.epsilon.test.builtins.CanAccessBuiltinsTests;
+
+public class EclCanAccessBuiltinsTests extends CanAccessBuiltinsTests {
+
+	@Override
+	protected IEolLibraryModule createModule() {
+		return new EclModule();
+	}
+
+	@Override
+	protected File getProgram() {
+		return FileUtil.getFile(("System.ecl"), EclCanAccessBuiltinsTests.class);
+	}
+
+	@Override
+	protected String getExpectedPrintedValue() {
+		return "Running ECL";
+	}
+}
