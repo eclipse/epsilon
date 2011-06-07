ceb5b0d8e0a985012eb218d1990934280d75cf23
agarciad <agarciad@b1e7822e-7b56-0410-88b3-db6ede196368>
2011-06-07 09:27:37 +0000

    [Eugenia] Support GMF figure templates
    In the GmfMap -> GmfGen step, some bits of Java code are generated and
    embedded in the GmfGen model. To customize this code, we need to use
    GMF figure templates.
    
    This commit adds support to Eugenia for GMF figure templates. Assume
    that the metamodel is in MyProject/model/a.emf. If the MyProject/templates-gmfmap
    folder exists, it will be searched for custom GMF figure templates.

--- plugins/org.eclipse.epsilon.eugenia/src/org/eclipse/epsilon/eugenia/GmfMap2GmfGenDelegate.java
+++ plugins/org.eclipse.epsilon.eugenia/src/org/eclipse/epsilon/eugenia/GmfMap2GmfGenDelegate.java
@@ -1,8 +1,14 @@
 package org.eclipse.epsilon.eugenia;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
@@ -18,21 +24,32 @@ public class GmfMap2GmfGenDelegate extends EugeniaActionDelegate {
 		// Transform GmfMap to GmfGen model
 		ResourceSet resourceSet = new ResourceSetImpl();
 		TransformToGenModelOperation transformToGenModelOperation = new TransformToGenModelOperation(resourceSet);
-		
+
 		TransformOptions options = transformToGenModelOperation.getOptions();
 		options.setGenerateRCP(false);
 		options.setUseMapMode(true);
 		options.setUseRuntimeFigures(true);
-		
+
+		final IFolder fGmfmapTemplateDir = getSelectedFile().getParent().getFolder(new Path("../templates-gmfmap"));
+		if (fGmfmapTemplateDir.exists()) {
+			try {
+				options.setFigureTemplatesPath(new URL(fGmfmapTemplateDir.getLocationURI().toString()));
+			} catch (MalformedURLException e) {
+				Activator.getDefault().getLog().log(new Status(
+					IStatus.ERROR, Activator.PLUGIN_ID, "Could not set the path to the figure templates", e));
+			}
+		}
+
 		try {
 			transformToGenModelOperation.loadMappingModel(URI.createURI(gmfFileSet.getGmfMapPath()), new NullProgressMonitor());
 			transformToGenModelOperation.loadGenModel(URI.createURI(gmfFileSet.getGenModelPath()), new NullProgressMonitor());
 		}
 		catch (Exception ex) {
-			ex.printStackTrace();
+			Activator.getDefault().getLog().log(new Status(
+				IStatus.ERROR, Activator.PLUGIN_ID, "Could not load the GmfMap and GmfGen models", ex));
 		}
 		transformToGenModelOperation.setGenURI(URI.createURI(gmfFileSet.getGmfGenPath()));
-		
+
 		transformToGenModelOperation.executeTransformation(new NullProgressMonitor());
 	}
 	
