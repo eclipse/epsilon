13a820d339d99b8396e047a728b777704d0cdb8d
agarciad <agarciad@b1e7822e-7b56-0410-88b3-db6ede196368>
2011-06-22 07:51:40 +0000

    Add test for Eugenia regression for support of GMF dynamic templates

--- tests/org.eclipse.epsilon.workflow.test/src/org/eclipse/epsilon/workflow/tasks/eugenia/EugeniaEmfInRootTest.java
+++ tests/org.eclipse.epsilon.workflow.test/src/org/eclipse/epsilon/workflow/tasks/eugenia/EugeniaEmfInRootTest.java
@@ -1,27 +1,31 @@
 /*******************************************************************************
- * Copyright (c) 2011 The University of York.
+ * Copyright (c) 2011 Antonio Garcia-Dominguez.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Antonio Garcia-Dominguez - initial API and implementation
  ******************************************************************************/
 package org.eclipse.epsilon.workflow.tasks.eugenia;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
+import org.eclipse.core.runtime.CoreException;
+import org.junit.Test;
 
 /**
- * Test suite for Eugenia.
+ * Tests that EuGENia works fine when the .emf is in the root folder of the project.
  */
-@RunWith(Suite.class)
-@SuiteClasses({
-	EugeniaExtraModelsTest.class,
-	EugeniaOptionalAttributeTest.class,
-	EugeniaRegressionTest.class,
-	EugeniaEmfInRootTest.class
-})
-public class EugeniaTestSuite {}
+public class EugeniaEmfInRootTest extends EugeniaTest {
+
+	private static final String CASE_NAME = "friends";
+
+	public EugeniaEmfInRootTest() {
+		super(CASE_NAME);
+	}
+
+	@Test
+	public void worksWithEmfInRoot() throws CoreException {
+		runAntTarget("emf-in-root");
+	}
+}
