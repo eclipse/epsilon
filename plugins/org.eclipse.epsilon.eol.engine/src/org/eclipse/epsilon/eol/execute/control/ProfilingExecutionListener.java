faa94a483a15589fd2fab8f321c26e8a80116783
dkolovos <dkolovos@b1e7822e-7b56-0410-88b3-db6ede196368>
2011-07-18 10:00:15 +0000

    Removed platform-specific launch configuration and batch files for generating Epsilon parsers (replaced by the new ANT macro)

--- plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/execute/control/ProfilingExecutionListener.java
+++ plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/execute/control/ProfilingExecutionListener.java
@@ -3,18 +3,26 @@ package org.eclipse.epsilon.eol.execute.control;
 import org.eclipse.epsilon.commons.parse.AST;
 import org.eclipse.epsilon.commons.profiling.FileMarker;
 import org.eclipse.epsilon.commons.profiling.Profiler;
+import org.eclipse.epsilon.commons.util.AstUtil;
 import org.eclipse.epsilon.eol.execute.context.IEolContext;
+import org.eclipse.epsilon.eol.parse.EolParser;
 
 public class ProfilingExecutionListener implements IExecutionListener {
 
 	@Override
 	public void aboutToExecute(AST ast, IEolContext context) {
-		Profiler.INSTANCE.start(ast.toString(), "", new FileMarker(ast.getFile(), ast.getLine(), ast.getColumn()));
+		if (AstUtil.getParentType(ast) == EolParser.BLOCK)
+			Profiler.INSTANCE.start(getLabel(ast), "", new FileMarker(ast.getFile(), ast.getLine(), ast.getColumn()));
 	}
 
 	@Override
 	public void finishedExecuting(AST ast, IEolContext context) {
-		Profiler.INSTANCE.stop(ast.toString());
+		if (AstUtil.getParentType(ast) == EolParser.BLOCK)
+			Profiler.INSTANCE.stop(getLabel(ast));
 	}
-
+	
+	protected String getLabel(AST ast) {
+		return "Line " + ast.getLine();
+	}
+	
 }
