49233a964160c1dfa753111e8196dafba9cd07e6
lrose <lrose@b1e7822e-7b56-0410-88b3-db6ede196368>
2011-09-05 10:56:02 +0000

    [EGL] Require EMF for Galileo or later (not Indigo).

--- plugins/org.eclipse.epsilon.egl.dt.traceability.fine.emf/src/org/eclipse/epsilon/egl/dt/traceability/fine/emf/trace/util/TraceSwitch.java
+++ plugins/org.eclipse.epsilon.egl.dt.traceability.fine.emf/src/org/eclipse/epsilon/egl/dt/traceability/fine/emf/trace/util/TraceSwitch.java
@@ -10,10 +10,10 @@
  */
 package org.eclipse.epsilon.egl.dt.traceability.fine.emf.trace.util;
 
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EPackage;
+import java.util.List;
 
-import org.eclipse.emf.ecore.util.Switch;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
 
 import org.eclipse.epsilon.egl.dt.traceability.fine.emf.trace.*;
 
@@ -30,7 +30,7 @@ import org.eclipse.epsilon.egl.dt.traceability.fine.emf.trace.*;
  * @see org.eclipse.epsilon.egl.dt.traceability.fine.emf.trace.TracePackage
  * @generated
  */
-public class TraceSwitch<T> extends Switch<T> {
+public class TraceSwitch<T> {
 	/**
 	 * The cached model package
 	 * <!-- begin-user-doc -->
@@ -52,16 +52,14 @@ public class TraceSwitch<T> extends Switch<T> {
 	}
 
 	/**
-	 * Checks whether this is a switch for the given package.
+	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @parameter ePackage the package in question.
-	 * @return whether this is a switch for the given package.
+	 * @return the first non-null result returned by a <code>caseXXX</code> call.
 	 * @generated
 	 */
-	@Override
-	protected boolean isSwitchFor(EPackage ePackage) {
-		return ePackage == modelPackage;
+	public T doSwitch(EObject theEObject) {
+		return doSwitch(theEObject.eClass(), theEObject);
 	}
 
 	/**
@@ -71,7 +69,26 @@ public class TraceSwitch<T> extends Switch<T> {
 	 * @return the first non-null result returned by a <code>caseXXX</code> call.
 	 * @generated
 	 */
-	@Override
+	protected T doSwitch(EClass theEClass, EObject theEObject) {
+		if (theEClass.eContainer() == modelPackage) {
+			return doSwitch(theEClass.getClassifierID(), theEObject);
+		}
+		else {
+			List<EClass> eSuperTypes = theEClass.getESuperTypes();
+			return
+				eSuperTypes.isEmpty() ?
+					defaultCase(theEObject) :
+					doSwitch(eSuperTypes.get(0), theEObject);
+		}
+	}
+
+	/**
+	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the first non-null result returned by a <code>caseXXX</code> call.
+	 * @generated
+	 */
 	protected T doSwitch(int classifierID, EObject theEObject) {
 		switch (classifierID) {
 			case TracePackage.TRACE: {
@@ -237,7 +254,6 @@ public class TraceSwitch<T> extends Switch<T> {
 	 * @see #doSwitch(org.eclipse.emf.ecore.EObject)
 	 * @generated
 	 */
-	@Override
 	public T defaultCase(EObject object) {
 		return null;
 	}
