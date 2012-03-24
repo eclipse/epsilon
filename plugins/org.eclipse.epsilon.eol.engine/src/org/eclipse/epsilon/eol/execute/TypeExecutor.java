5b21642797e1af95fe8538948ffa1a09131f2023
dkolovos <dkolovos@b1e7822e-7b56-0410-88b3-db6ede196368>
2012-03-24 00:19:56 +0000

    Changed TypeExecutor to return null instead of throwing an exception when it cannot resolve a type.

--- plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/execute/TypeExecutor.java
+++ plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/execute/TypeExecutor.java
@@ -93,8 +93,8 @@ public class TypeExecutor extends AbstractExecutor{
 		
 		//return new EolNativeType(ast, context);
 		
-		throw new EolTypeNotFoundException(typeName, ast);
-		
+		// throw new EolTypeNotFoundException(typeName, ast);
+		return null;
 	}
 
 }
