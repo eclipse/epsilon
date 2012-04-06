a2a11229e664bede560ff1f358aaa37ecb6283a5
dkolovos <dkolovos@b1e7822e-7b56-0410-88b3-db6ede196368>
2012-04-06 13:40:58 +0000

    Added missing copyright/license headers

--- plugins/org.eclipse.epsilon.eunit.engine/src/org/eclipse/epsilon/eunit/operations/TreeEqualityAssertionOperation.java
+++ plugins/org.eclipse.epsilon.eunit.engine/src/org/eclipse/epsilon/eunit/operations/TreeEqualityAssertionOperation.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Antonio García-Domínguez
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Antonio García Domínguez - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.epsilon.eunit.operations;
 
 import java.io.File;
