39e3f62517ffbeb0e9a388f9db33091ace6b7d71
dkolovos <dkolovos@b1e7822e-7b56-0410-88b3-db6ede196368>
2013-04-07 16:37:25 +0000

    Added a separate image registry for image descriptors

--- plugins/org.eclipse.epsilon.common.dt/src/org/eclipse/epsilon/common/dt/AbstractEpsilonUIPlugin.java
+++ plugins/org.eclipse.epsilon.common.dt/src/org/eclipse/epsilon/common/dt/AbstractEpsilonUIPlugin.java
@@ -1,45 +1,46 @@
 package org.eclipse.epsilon.common.dt;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Collections;
 import java.util.HashMap;
 
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.epsilon.common.dt.util.LogUtil;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.ui.internal.util.BundleUtility;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 public abstract class AbstractEpsilonUIPlugin extends AbstractUIPlugin implements EpsilonPlugin {
 	
 	protected static HashMap<Class<?>, AbstractEpsilonUIPlugin> plugins = new HashMap<Class<?>, AbstractEpsilonUIPlugin>();
+	protected ImageRegistry imageDescriptorRegistry;
 	
 	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugins.put(this.getClass(), this);
+		imageDescriptorRegistry = createImageRegistry();
 	}
 	
 	@Override
 	public void stop(BundleContext context) throws Exception {
 		super.stop(context);
 		plugins.remove(this.getClass());
+		imageDescriptorRegistry.dispose();
 	}
 	
 	public ImageDescriptor getImageDescriptor(String path) {
 		
-		ImageDescriptor imageDescriptor = getImageRegistry().getDescriptor(path);
+		ImageDescriptor imageDescriptor = imageDescriptorRegistry.getDescriptor(path);
 		
 		if (imageDescriptor == null) {
 			URL url = FileLocator.find(getBundle(), new Path(path), Collections.emptyMap());
 			if (url != null) {
 				imageDescriptor = ImageDescriptor.createFromURL(url);
-				if (imageDescriptor != null) getImageRegistry().put(path, imageDescriptor);
+				if (imageDescriptor != null) imageDescriptorRegistry.put(path, imageDescriptor);
 			}
 		}
 		
