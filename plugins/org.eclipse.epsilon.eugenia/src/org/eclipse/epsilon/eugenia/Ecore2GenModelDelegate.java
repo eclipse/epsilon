4d9c9363d9418ed99da9bda44430d0dc8940ce7e
dkolovos <dkolovos@b1e7822e-7b56-0410-88b3-db6ede196368>
2013-04-20 00:48:31 +0000

    Added support for external GenPackages to the Eugenia Ecore2GenModel transformation.

--- plugins/org.eclipse.epsilon.eugenia/src/org/eclipse/epsilon/eugenia/Ecore2GenModelDelegate.java
+++ plugins/org.eclipse.epsilon.eugenia/src/org/eclipse/epsilon/eugenia/Ecore2GenModelDelegate.java
@@ -11,10 +11,20 @@
 package org.eclipse.epsilon.eugenia;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
 import org.eclipse.emf.codegen.ecore.genmodel.GenModelPackage;
+import org.eclipse.emf.codegen.ecore.genmodel.GenPackage;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EAnnotation;
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.epsilon.common.dt.util.LogUtil;
 import org.eclipse.epsilon.emc.emf.EmfModel;
 import org.eclipse.epsilon.eol.IEolExecutableModule;
 import org.eclipse.epsilon.eol.execute.context.Variable;
@@ -45,7 +55,7 @@ public class Ecore2GenModelDelegate extends GuardedEcoreModelGenerationDelegate 
 		List<IModel> models = new ArrayList<IModel>();
 
 		// Bug 360629: to avoid breaking code, we keep the old 'Ecore' name, but we add an 'ECore' alias for consistency
-		final EmfModel ecoreModel = loadModel("Ecore", gmfFileSet.getEcorePath(), EcorePackage.eINSTANCE.getNsURI(), true, false, true);
+		final EmfModel ecoreModel = loadModel("Ecore", gmfFileSet.getEcorePath(), EcorePackage.eINSTANCE.getNsURI(), true, false, false);
 		final IModel ecoreModelAlias = new ModelReference(ecoreModel);
 		ecoreModelAlias.setName("ECore");
 
@@ -59,6 +69,33 @@ public class Ecore2GenModelDelegate extends GuardedEcoreModelGenerationDelegate 
 	@Override
 	public List<Variable> getExtraVariables() {
 		ArrayList<Variable> variables = new ArrayList<Variable>();
+		HashMap<String, GenPackage> usedGenPackages = new HashMap<String, GenPackage>();
+		
+		try {
+			ResourceSet eCoreResourceSet = new ResourceSetImpl();
+			Resource eCoreResource = eCoreResourceSet.createResource(URI.createURI(gmfFileSet.getEcorePath()));
+			eCoreResource.load(null);
+			EPackage ePackage = (EPackage) eCoreResource.getContents().get(0);
+			for (EAnnotation eAnnotation : ePackage.getEAnnotations()) {
+				if (eAnnotation.getSource().equalsIgnoreCase("emf.gen.usedGenPackage")) {
+					ResourceSet genModelResourceSet = new ResourceSetImpl();
+					Resource genModelResource = genModelResourceSet.createResource(URI.createURI(eAnnotation.getDetails().get("genModel"), false));
+					genModelResource.load(null);
+					GenModel genModel = (GenModel) genModelResource.getContents().get(0);
+					for (GenPackage genPackage : genModel.getGenPackages()) {
+						String usedGenPackageName = eAnnotation.getDetails().get("name");
+						if (genPackage.getEcorePackage().getName().equals(usedGenPackageName)) {
+							usedGenPackages.put(usedGenPackageName, genPackage);
+						}
+					}
+				}
+			}
+		}
+		catch (Exception ex) {
+			LogUtil.log(ex);
+		}
+		
+		variables.add(Variable.createReadOnlyVariable("usedGenPackages", usedGenPackages));
 		variables.add(CopyrightProvider.getCopyrightVariable(getSelectedFile()));
 		variables.add(Variable.createReadOnlyVariable("pluginName", getSelectedFile().getProject().getName()));
 		variables.add(Variable.createReadOnlyVariable("foreignModel", getSelectedFile().getName()));
