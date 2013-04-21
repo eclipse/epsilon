9047135197f81069ebaf749c218e263f7c05f6d9
dkolovos <dkolovos@b1e7822e-7b56-0410-88b3-db6ede196368>
2013-04-21 00:57:44 +0000

    Added package context-less operation for annotation externalisation

--- plugins/org.eclipse.epsilon.eugenia/src/org/eclipse/epsilon/eugenia/operationcontributors/ContextlessOperationContributor.java
+++ plugins/org.eclipse.epsilon.eugenia/src/org/eclipse/epsilon/eugenia/operationcontributors/ContextlessOperationContributor.java
@@ -1,6 +1,7 @@
 package org.eclipse.epsilon.eugenia.operationcontributors;
 
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
 import org.eclipse.epsilon.eol.execute.operations.contributors.OperationContributor;
 import org.eclipse.epsilon.eol.models.IModel;
@@ -11,6 +12,16 @@ public class ContextlessOperationContributor extends OperationContributor {
 	@Override
 	public boolean contributesTo(Object target) {
 		return target instanceof EolNoTypeInstance;
+	}
+	
+	public Object _package(String name) throws Exception {
+		IModel ecoreModel = context.getModelRepository().getModelByName("Ecore");
+		for (Object o : ecoreModel.getAllOfType("EPackage")) {
+			if (((EPackage) o).getName().equals(name)) {
+				return o;
+			}
+		}
+		throw new EolRuntimeException("Package " + name + " not found in the Ecore metamodel");
 	}
 	
 	public Object _class(String name) throws Exception {
