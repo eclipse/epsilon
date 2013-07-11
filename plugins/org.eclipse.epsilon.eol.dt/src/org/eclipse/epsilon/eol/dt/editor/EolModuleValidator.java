10886fc19551681cfd0e398cb05dd89cd89dbbfa
dkolovos <dkolovos@b1e7822e-7b56-0410-88b3-db6ede196368>
2013-07-11 14:28:20 +0000

    Tidied-up EGL module validator and added an EOL module validator that checks for statements after the first operation.

--- plugins/org.eclipse.epsilon.eol.dt/src/org/eclipse/epsilon/eol/dt/editor/EolModuleValidator.java
+++ plugins/org.eclipse.epsilon.eol.dt/src/org/eclipse/epsilon/eol/dt/editor/EolModuleValidator.java
@@ -1,4 +1,4 @@
-package org.eclipse.epsilon.egl.dt.editor;
+package org.eclipse.epsilon.eol.dt.editor;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -9,13 +9,13 @@ import org.eclipse.epsilon.common.module.IModuleValidator;
 import org.eclipse.epsilon.common.module.ModuleMarker;
 import org.eclipse.epsilon.common.module.ModuleMarker.Severity;
 import org.eclipse.epsilon.common.parse.AST;
-import org.eclipse.epsilon.egl.EglTemplateFactoryModuleAdapter;
+import org.eclipse.epsilon.eol.EolModule;
 import org.eclipse.epsilon.eol.parse.EolParser;
 
-public class EglModuleValidator implements IModuleValidator {
+public class EolModuleValidator implements IModuleValidator {
 	
 	protected boolean appliesTo(IModule module) {
-		return module instanceof EglTemplateFactoryModuleAdapter;
+		return module.getClass() == EolModule.class;
 	}
 	
 	@Override
@@ -29,8 +29,8 @@ public class EglModuleValidator implements IModuleValidator {
 		
 		for (AST ast : module.getAst().getChildren()) {
 			if (ast.getType() != EolParser.HELPERMETHOD && ast.getType() != EolParser.BLOCK) {
-				if (firstHelper != null && !isEmptyPrintStatement(ast)) {
-					markers.add(new ModuleMarker(null, firstHelper.getRegion(), "All loose statements and textual content after the first operation will be ignored at runtime.", Severity.Warning));
+				if (firstHelper != null) {
+					markers.add(new ModuleMarker(null, ast.getRegion(), "All statements after the first operation will be ignored at runtime.", Severity.Warning));
 				}
 			}
 			else if (ast.getType() == EolParser.HELPERMETHOD) {
@@ -42,12 +42,4 @@ public class EglModuleValidator implements IModuleValidator {
 		
 	}
 	
-	protected boolean isEmptyPrintStatement(AST ast) {
-		if (ast.getText().equals(".") && ast.getChild(0).getText().equals("out") && ast.getChild(1).getText().equals("print")) {
-			String text = ast.getChild(1).getFirstChild().getFirstChild().getText();
-			return text.trim().equals("\\n") || text.trim().isEmpty();
-		}
-		return false;
-	}
-
 }
