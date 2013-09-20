6cac2add83108545cb6657f034527630722aff7f
dkolovos <dkolovos@b1e7822e-7b56-0410-88b3-db6ede196368>
2013-09-20 08:09:16 +0000

    Eclipse project metadata importer

---
+++ examples/org.ossmeter.repository.model.eclipse.importer/src/org/ossmeter/repository/model/eclipse/importer/EclipseProjectImporter.java
@@ -0,0 +1,11 @@
+package org.ossmeter.repository.model.eclipse.importer;
+
+import org.ossmeter.repository.model.eclipseforge.EclipseForgeProject;
+
+public class EclipseProjectImporter {
+	
+	public EclipseForgeProject importProject(String projectId) {
+		return null;
+	}
+	
+}
