eeb1578ddd6495799eb2c1bb94b17f50a8102dd4
dkolovos <dkolovos@b1e7822e-7b56-0410-88b3-db6ede196368>
2013-12-03 23:24:07 +0000

    Added an epsilon.java.executeStaticMethod ANT task - mainly for setting up Xtext-based languages outside Eclipse

---
+++ plugins/org.eclipse.epsilon.workflow/ant/org/eclipse/epsilon/workflow/tasks/ExecuteStaticMethodTask.java
@@ -0,0 +1,45 @@
+package org.eclipse.epsilon.workflow.tasks;
+
+import java.lang.reflect.Method;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+public class ExecuteStaticMethodTask extends Task {
+	
+	protected String javaClass;
+	protected String method;
+	
+	public static void foo() {
+		System.out.println("Foo invoked");
+	}
+	
+	@Override
+	public void execute() throws BuildException {
+		try {
+			for (Method m : Class.forName(javaClass).getMethods()) {
+				if (m.getName().equals(method) && m.getParameterTypes().length == 0) {
+					m.invoke(null, new Object[]{});
+				}
+			}
+		} catch (Exception e) {
+			throw new BuildException(e);
+		}
+	}
+	
+	public String getJavaClass() {
+		return javaClass;
+	}
+	
+	public void setJavaClass(String javaClass) {
+		this.javaClass = javaClass;
+	}
+	
+	public String getMethod() {
+		return method;
+	}
+	
+	public void setMethod(String method) {
+		this.method = method;
+	}
+}
