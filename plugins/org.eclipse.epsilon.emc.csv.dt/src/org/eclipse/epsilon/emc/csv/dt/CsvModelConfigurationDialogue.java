d81295d9678b9a4c3f04d76ccac23a75fabcedab
dkolovos <dkolovos@b1e7822e-7b56-0410-88b3-db6ede196368>
2014-02-13 20:46:49 +0000

    Applied patch 237904 of bug #422937 (extended CSV support)

--- plugins/org.eclipse.epsilon.emc.csv.dt/src/org/eclipse/epsilon/emc/csv/dt/CsvModelConfigurationDialogue.java
+++ plugins/org.eclipse.epsilon.emc.csv.dt/src/org/eclipse/epsilon/emc/csv/dt/CsvModelConfigurationDialogue.java
@@ -16,12 +16,17 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Text;
 
 public class CsvModelConfigurationDialogue extends AbstractCachedModelConfigurationDialog {
 
 	private Text fileText;
+	private Text fieldSeparatorText;
+	private Button knownHeadersBtn;
+	private Button varargsHeadersBtn;
 	
 	@Override
 	protected String getModelName() {
@@ -37,32 +42,70 @@ public class CsvModelConfigurationDialogue extends AbstractCachedModelConfigurat
 	protected void createGroups(Composite control) {
 		super.createGroups(control);
 		createFileGroup(control);
+		createLoadStoreOptionsGroup(control);
+		createCsvGroup(control);
 	}
 
 	private void createFileGroup(Composite parent) {
-		final Composite group = createGroupContainer(parent, "Files", 3);
+		final Composite groupContent = createGroupContainer(parent, "Files", 3);
 		
-		final Label modelFileLabel = new Label(group, SWT.NONE);
+		final Label modelFileLabel = new Label(groupContent, SWT.NONE);
 		modelFileLabel.setText("Model file: ");
 		
-		fileText = new Text(group, SWT.BORDER);
+		fileText = new Text(groupContent, SWT.BORDER);
 		fileText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		
-		final Button browseFile = new Button(group, SWT.NONE);
+		final Button browseFile = new Button(groupContent, SWT.NONE);
 		browseFile.setText("Browse Workspace...");
 		browseFile.addListener(SWT.Selection, new BrowseWorkspaceForModelsListener(fileText, "CSV files in the workspace", "Select a CSV file"));
+		
 	}
+	
+	protected void createCsvGroup(Composite parent) {
+		final Composite groupContent = createGroupContainer(parent, "CVS", 4);
+		
+		final Label modelFieldSeparatorLabel = new Label(groupContent, SWT.NONE);
+		modelFieldSeparatorLabel.setText("Field Separator: ");
+		fieldSeparatorText = new Text(groupContent, SWT.BORDER);
+		fieldSeparatorText.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_BEGINNING));
+		
+		knownHeadersBtn = new Button(groupContent, SWT.CHECK);
+		knownHeadersBtn.setText("Known Headers");
+		knownHeadersBtn.addListener(SWT.Selection, new Listener() {
+			
+			@Override
+			public void handleEvent(Event event) {
+				System.out.println("knownHeadersBtn Selected");
+				varargsHeadersBtn.setEnabled(knownHeadersBtn.getSelection());
+			}
+		});
+		varargsHeadersBtn = new Button(groupContent, SWT.CHECK);
+		varargsHeadersBtn.setText("Varargs Headers");
+	}
+	
+	
 
 	@Override
 	protected void loadProperties() {
 		super.loadProperties();
-		if (properties == null) return;
-		fileText.setText(properties.getProperty(CsvModel.PROPERTY_FILE));
+		if (properties == null) {
+			fieldSeparatorText.setText(",");
+			knownHeadersBtn.setSelection(true);
+		} else {
+			fileText.setText(properties.getProperty(CsvModel.PROPERTY_FILE));
+			fieldSeparatorText.setText(properties.getProperty(CsvModel.PROPERTY_FIELD_SEPARATOR));
+			knownHeadersBtn.setSelection(properties.getBooleanProperty(CsvModel.PROPERTY_HAS_KNOWN_HEADERS, true));
+			varargsHeadersBtn.setSelection(properties.getBooleanProperty(CsvModel.PROPERTY_HAS_VARARGS_HEADERS, false));
+			varargsHeadersBtn.setEnabled(knownHeadersBtn.getSelection());
+		}
 	}
 
 	@Override
 	protected void storeProperties() {
 		super.storeProperties();
 		properties.setProperty(CsvModel.PROPERTY_FILE, fileText.getText());
+		properties.setProperty(CsvModel.PROPERTY_FIELD_SEPARATOR, fieldSeparatorText.getText());
+		properties.setProperty(CsvModel.PROPERTY_HAS_KNOWN_HEADERS, String.valueOf(knownHeadersBtn.getSelection()));
+		properties.setProperty(CsvModel.PROPERTY_HAS_VARARGS_HEADERS, String.valueOf(varargsHeadersBtn.getSelection()));
 	}
 }
