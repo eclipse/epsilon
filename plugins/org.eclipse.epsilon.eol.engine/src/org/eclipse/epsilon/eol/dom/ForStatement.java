cf274ca30e403b01e62f351a378371767f67c22f
dkolovos <dkolovos@b1e7822e-7b56-0410-88b3-db6ede196368>
2014-03-26 18:46:47 +0000

    Added SpecialAssginmentStatement

--- plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/dom/ForStatement.java
+++ plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/dom/ForStatement.java
@@ -1,5 +1,19 @@
 package org.eclipse.epsilon.eol.dom;
 
+import org.eclipse.epsilon.common.parse.AST;
+
 public class ForStatement extends Statement {
+	
+	public VariableDeclaration getIterator() {
+		return (VariableDeclaration) getFirstChild();
+	}
+	
+	public Expression getIterated() {
+		return (Expression) getSecondChild();
+	}
+	
+	public AST getBody() {
+		return getThirdChild();
+	}
 	
 }
--- plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/dom/IfStatement.java
+++ plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/dom/IfStatement.java
@@ -1,5 +1,22 @@
 package org.eclipse.epsilon.eol.dom;
 
-public class IfStatement extends Statement {
+import org.eclipse.epsilon.common.parse.AST;
 
+public class IfStatement extends Statement {
+	
+	public Expression getCondition() {
+		return (Expression) getFirstChild();
+	}
+	
+	public AST getThen() {
+		return getSecondChild();
+	}
+	
+	public AST getElse() {
+		if (getChildCount() == 3) {
+			return getChild(2);
+		}
+		else return null;
+	}
+	
 }
