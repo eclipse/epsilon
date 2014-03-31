1e1e8668d19f393ac41ac1b3e80de73af68f7fdd
mbezemer <mbezemer@b1e7822e-7b56-0410-88b3-db6ede196368>
2014-03-31 14:10:30 +0000

    Put helper moethod to load the renamed model back (removed in r2492)
    This fixes problems with external cross/proxy references to objects in other files/models.
    These problems caused the test suite to fail 8 related tests.
    
    Compared to its previous implementation it uses IConcordanceModel to obtain the model content.
    External cross/proxy references are fixed by replacing the 'renamed URI' with the 'original URI'.
    So these references are pointing to the correct external objects again.

--- plugins/org.eclipse.epsilon.concordance.core/src/org/eclipse/epsilon/concordance/model/CrossReferenceReconciler.java
+++ plugins/org.eclipse.epsilon.concordance.core/src/org/eclipse/epsilon/concordance/model/CrossReferenceReconciler.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2009 The University of York.
+ * Copyright (c) 2014 University of Twente.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +8,7 @@
  * 
  * Contributors:
  *     Louis Rose - initial API and implementation
+ *     Maarten Bezemer - Make use of IConcordanceModel methods
  ******************************************************************************
  *
  * $Id$
@@ -14,15 +16,22 @@
 package org.eclipse.epsilon.concordance.model;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature.Setting;
+import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.epsilon.common.dt.util.LogUtil;
 
 public class CrossReferenceReconciler {
 
 	private final IConcordanceModel reconcilee;
-	
+
 	public CrossReferenceReconciler(IConcordanceModel reconcilee) {
 		this.reconcilee = reconcilee;
 	}
@@ -34,21 +43,20 @@ public class CrossReferenceReconciler {
 			LogUtil.log("Error encountered whilst trying to reconcile " + reconcilee + " by changing " + original + " to " + moved, e);
 		}
 	}
-	
+
 	private void reconcile(Renaming renaming) throws IOException {
 		final Resource res;
 
 		if (renaming.isRenamed(reconcilee)) {
-			res = renaming.renamed.getEmfResource(false);
+			res = renaming.loadRenamedResource();
 
 			EcoreUtil.resolveAll(res);
-
 			res.setURI(renaming.renamed.getUri());
 
 		} else {
 			res = reconcilee.getEmfResource(false);
 
-			final Resource renamed = renaming.renamed.getEmfResource(false);
+			final Resource renamed = renaming.loadRenamedResource();
 
 			res.getResourceSet().getResources().add(renamed);
 			EcoreUtil.resolveAll(res);
@@ -60,16 +68,53 @@ public class CrossReferenceReconciler {
 	}
 
 	private static class Renaming {
-		
+
 		public final IConcordanceModel original, renamed;
-		
+
 		public Renaming(IConcordanceModel original, IConcordanceModel renamed) {
 			this.original = original;
 			this.renamed  = renamed;
 		}
-		
+
 		private boolean isRenamed(IConcordanceModel model) {
 			return renamed.equals(model);
+		}
+
+		/**
+		 * Provides (a copy of) the renamed model and fixes the broken external
+		 * cross/proxy references (if there are any), caused by renaming the model.
+		 *
+		 * @return a resource with the copied contents of the renamed model
+		 * @throws IOException
+		 *             when obtaining 'renamed model' failed
+		 */
+		public Resource loadRenamedResource() throws IOException {
+			// Create a copy of the contents of the renamed model
+			final Resource renamedResource = renamed.getEmfResource(false);
+			final Collection<EObject> copiedContents = EcoreUtil
+					.copyAll(renamedResource.getContents());
+
+			Map<EObject, Collection<Setting>> map = EcoreUtil.ExternalCrossReferencer
+					.find(copiedContents);
+			for (EObject o : map.keySet()) {
+				InternalEObject eObject = (InternalEObject) o;
+				// Find all proxied cross references
+				if (eObject.eIsProxy()) {
+					// Correct URI: replace renamed-based URI with
+					// original-based URI (so all references are intact again)
+					URI uri = eObject.eProxyURI();
+					uri = uri.deresolve(renamed.getUri());
+					uri = uri.resolve(original.getUri());
+					eObject.eSetProxyURI(uri);
+				}
+			}
+
+			// Put copied content into a new resource
+			final Resource resource = new ResourceSetImpl()
+					.createResource(original.getUri());
+			resource.getContents().addAll(copiedContents);
+
+			return resource;
 		}
 	}
 }
