8b7bd8823c22d6b4fb7de03f274589ba7e7a8f03
dkolovos <dkolovos@b1e7822e-7b56-0410-88b3-db6ede196368>
2014-04-06 00:36:29 +0000

    Added EML tree merging test

---
+++ tests/org.eclipse.epsilon.eml.engine.test.acceptance/src/org/eclipse/epsilon/eml/engine/test/acceptance/TestXmlTreeMerging.java
@@ -0,0 +1,79 @@
+package org.eclipse.epsilon.eml.engine.test.acceptance;
+
+import java.io.File;
+import java.util.HashMap;
+
+import org.eclipse.epsilon.ecl.EclModule;
+import org.eclipse.epsilon.emc.emf.xml.XmlModel;
+import org.eclipse.epsilon.emc.plainxml.PlainXmlModel;
+import org.eclipse.epsilon.eml.EmlModule;
+import org.eclipse.epsilon.eol.execute.context.Variable;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestXmlTreeMerging {
+	
+	protected EclModule eclModule = null;
+	protected EmlModule emlModule = null;
+	protected PlainXmlModel mergedModel = null;
+	
+	protected HashMap<String, Object> prepost;
+	
+	@Before
+	public void setup() throws Exception {
+		
+		PlainXmlModel leftModel = loadXmlModel("Left", "left.xml");
+		PlainXmlModel rightModel = loadXmlModel("Right", "right.xml");
+		leftModel.getAliases().add("Source");
+		rightModel.getAliases().add("Right");
+		
+		eclModule = new EclModule();
+		eclModule.parse(getClass().getResource("trees.ecl").toURI());
+		prepost = new HashMap<String, Object>();
+		eclModule.getContext().getFrameStack().put(Variable.createReadOnlyVariable("prepost", prepost));
+		eclModule.getContext().getModelRepository().addModel(leftModel);
+		eclModule.getContext().getModelRepository().addModel(rightModel);
+		eclModule.execute();
+		
+		mergedModel = loadXmlModel("Merged", "merged.xml", false);
+		emlModule = new EmlModule();
+		emlModule.parse(getClass().getResource("trees.eml").toURI());
+		emlModule.getContext().getFrameStack().put(Variable.createReadOnlyVariable("prepost", prepost));
+		emlModule.getContext().getModelRepository().addModel(leftModel);
+		emlModule.getContext().getModelRepository().addModel(rightModel);
+		emlModule.getContext().setMatchTrace(eclModule.getContext().getMatchTrace().getReduced());
+		emlModule.getContext().getModelRepository().addModel(mergedModel);
+		emlModule.execute();
+	}
+	
+	@Test
+	public void testCorrectNumberOfElements() throws Exception {
+		assertEquals(5, mergedModel.getAllOfKind("t_tree").size());
+	}
+	
+	@Test
+	public void testPreExecuted() {
+		assertEquals("pre", prepost.get("pre"));		
+	}
+	
+	@Test
+	public void testPostExecuted() {
+		assertEquals("post", prepost.get("post"));		
+	}
+	
+	protected PlainXmlModel loadXmlModel(String name, String fileName, boolean readOnLoad) throws Exception {
+		PlainXmlModel model = new PlainXmlModel();
+		model.setName(name);
+		model.setFile(new File(getClass().getResource(fileName).toURI()));
+		model.setReadOnLoad(readOnLoad);
+		model.load();
+		return model;
+	}
+	
+	protected PlainXmlModel loadXmlModel(String name, String fileName) throws Exception {
+		return loadXmlModel(name, fileName, true);
+	}
+	
+}
---
+++ tests/org.eclipse.epsilon.eml.engine.test.acceptance/src/org/eclipse/epsilon/eml/engine/test/acceptance/trees.ecl
@@ -0,0 +1,31 @@
+pre {
+	prepost.put("pre", "pre");
+}
+
+post {
+	prepost.put("post", "post");
+}
+
+rule Root2Root 
+	match l : Left!t_tree
+	with r : Right!t_tree {
+	
+	guard : l.parent.isUndefined() 
+		and r.parent.isUndefined()
+	
+	compare : true
+		
+}
+
+rule Tree2Tree
+	match l : Left!t_tree
+	with r : Right!t_tree {
+
+	guard : l.a_label <> "t5" 
+		and l.parent.isDefined()
+		and r.parent.isDefined()
+	
+	compare: l.a_label == r.a_label 
+		and l.parent.matches(r.parent)
+	
+}
