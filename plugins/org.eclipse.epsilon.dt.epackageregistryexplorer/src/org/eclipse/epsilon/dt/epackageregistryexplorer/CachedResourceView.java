c1ff883bb25ef18647f11f83e4781bc7e06c3a26
dkolovos <dkolovos@b1e7822e-7b56-0410-88b3-db6ede196368>
2014-07-09 20:52:28 +0000

    Minor cosmetic tweaks in the new Cached Models view

--- plugins/org.eclipse.epsilon.dt.epackageregistryexplorer/src/org/eclipse/epsilon/dt/epackageregistryexplorer/CachedResourceView.java
+++ plugins/org.eclipse.epsilon.dt.epackageregistryexplorer/src/org/eclipse/epsilon/dt/epackageregistryexplorer/CachedResourceView.java
@@ -1,35 +1,45 @@
 package org.eclipse.epsilon.dt.epackageregistryexplorer;
 
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.epsilon.emc.emf.CachedResourceSet;
 import org.eclipse.epsilon.emc.emf.CachedResourceSet.Cache;
 import org.eclipse.epsilon.emc.emf.CachedResourceSet.Cache.CacheItem;
 import org.eclipse.jface.action.Action;
-import org.eclipse.jface.viewers.ISelectionProvider;
-import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.jface.viewers.TreeSelection;
-import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.part.ViewPart;
 
 public class CachedResourceView extends ViewPart {
 	
-	protected TreeViewer cacheViewer = null;
+	protected TableViewer cacheViewer = null;
 	
 	@Override
 	public void createPartControl(Composite parent) {
-		cacheViewer = new TreeViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		cacheViewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
 		cacheViewer.setContentProvider(new CachedResourceViewerContentProvider());
 		cacheViewer.setLabelProvider(new CachedResoureViewerLabelProvider());
 		cacheViewer.setSorter(new AlphabeticalSorter());
 		cacheViewer.setInput(CachedResourceSet.getCache());
 		
+		TableColumn column = new TableColumn(cacheViewer.getTable(), SWT.FULL_SELECTION);
+	    column.setText("Resource");
+	    column.setWidth(300);
+	    
+	    column = new TableColumn(cacheViewer.getTable(), SWT.FULL_SELECTION);
+	    column.setText("Garbage-collected");
+	    column.setWidth(120);
+		
+	    cacheViewer.getTable().setHeaderVisible(true);
+	    cacheViewer.getTable().setLinesVisible(true);
+	    
+	    
 		IActionBars bars = getViewSite().getActionBars();
 		bars.getToolBarManager().add(new RefreshAction());
 		bars.getToolBarManager().add(new ClearAction());
@@ -41,8 +51,6 @@ public class CachedResourceView extends ViewPart {
 	}
 	
 	class RefreshAction extends Action {
-		
-		protected boolean running = false;
 		
 		public RefreshAction() {
 			this.setText("Refresh");
@@ -57,8 +65,6 @@ public class CachedResourceView extends ViewPart {
 	
 	class ClearAction extends Action {
 		
-		protected boolean running = false;
-		
 		public ClearAction() {
 			this.setText("Clear cache");
 			this.setImageDescriptor(Activator.getDefault().getImageDescriptor("icons/clear.gif"));
@@ -71,23 +77,26 @@ public class CachedResourceView extends ViewPart {
 		}		
 	}
 	
-	public class CachedResoureViewerLabelProvider extends LabelProvider {
+	public class CachedResoureViewerLabelProvider extends LabelProvider implements ITableLabelProvider {
 		
 		protected Image image = Activator.getDefault().getImageDescriptor("icons/emfmodel.gif").createImage();
 		
+
 		@Override
-		public Image getImage(Object element) {
-			return image;
+		public Image getColumnImage(Object element, int columnIndex) {
+			if (columnIndex == 0) return image;
+			return null;
 		}
-		
+
 		@Override
-		public String getText(Object element) {
-			return ((CacheItem) element).getUri().toString();
+		public String getColumnText(Object element, int columnIndex) {
+			if (columnIndex == 0) return ((CacheItem) element).getUri().toString();
+			else return (((CacheItem) element).getResource() == null) + "";
 		}
 		
 	}
 	
-	public class CachedResourceViewerContentProvider implements ITreeContentProvider {
+	public class CachedResourceViewerContentProvider implements IStructuredContentProvider {
 
 		@Override
 		public void dispose() {
@@ -102,21 +111,6 @@ public class CachedResourceView extends ViewPart {
 		@Override
 		public Object[] getElements(Object inputElement) {
 			return CachedResourceSet.getCache().getItems().toArray(new Cache.CacheItem[]{});
-		}
-
-		@Override
-		public Object[] getChildren(Object parentElement) {
-			return null;
-		}
-
-		@Override
-		public Object getParent(Object element) {
-			return null;
-		}
-
-		@Override
-		public boolean hasChildren(Object element) {
-			return false;
 		}
 		
 	}
