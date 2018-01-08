85b7e16750b79341c5b77cf64c5ea7d254fdbe93
Antonio García-Domínguez <nyoescape@gmail.com>
2018-01-08 16:56:07 +0000

    Add accessor and reuse super ctor in EolUndefinedVariableException

--- plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/exceptions/EolUndefinedVariableException.java
+++ plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/exceptions/EolUndefinedVariableException.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2008 The University of York.
+ * Copyright (c) 2008-2018 The University of York, Aston University.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Dimitrios Kolovos - initial API and implementation
+ *     Antonio Garcia-Dominguez - reuse EolRuntimeException ctor, add accessor
  ******************************************************************************/
 package org.eclipse.epsilon.eol.exceptions;
 
@@ -14,17 +15,16 @@ import org.eclipse.epsilon.common.module.ModuleElement;
 
 public class EolUndefinedVariableException extends EolRuntimeException {
 
-	protected String variableName = "";
-	
+	private static final long serialVersionUID = 1L;
+	private final String variableName;
+
 	public EolUndefinedVariableException(String variableName, ModuleElement ast) {
-		super();
+		super("Undefined variable, type or model: '" + variableName + "'", ast);
 		this.variableName = variableName;
-		this.ast = ast;
 	}
-	
-	@Override
-	public String getReason(){
-		return "Undefined variable, type or model: '" + variableName + "'";
+
+	public String getVariableName() {
+		return variableName;
 	}
-	
+
 }
