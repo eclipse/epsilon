af63cd8a98c5a94afe4a922d17a8599c79c80b5f
Dimitris Kolovos <dkolovos@cs.york.ac.uk>
2018-07-08 12:47:35 +0100

    Added test with fully qqualified IDs and templates

--- tests/org.eclipse.epsilon.flexmi.test/src/org/eclipse/epsilon/flexmi/test/IdTests.java
+++ tests/org.eclipse.epsilon.flexmi.test/src/org/eclipse/epsilon/flexmi/test/IdTests.java
@@ -1,15 +1,25 @@
 package org.eclipse.epsilon.flexmi.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
+import org.eclipse.epsilon.flexmi.FlexmiResource;
 import org.junit.Test;
 
 public class IdTests extends FlexmiTests {
 	
-	
 	@Test
 	public void testNoWarnings() throws Exception {
 		assertEquals(0, loadResource("standalone/valid-model-with-fully-qualified-ids.flexmi").getWarnings().size());
+	}
+	
+	@Test
+	public void testTemplatesAndIds() throws Exception {
+		FlexmiResource resource =  loadResource("templates/model-with-template-and-fully-qualified-ids.flexmi");
+		assertNotNull(resource.getEObject("p1.C1"));
+		assertNotNull(resource.getEObject("p1.C2"));
+		assertNull(resource.getEObject("p1.C"));
 	}
 	
 	@Test
