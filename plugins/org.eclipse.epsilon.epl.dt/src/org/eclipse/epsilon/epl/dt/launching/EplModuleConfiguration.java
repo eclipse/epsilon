b0a579f455b40dec369bcb3ee37f86ceb935b020
Horacio Hoyos Rodriguez <arcanefoam@gmail.com>
2018-09-11 17:13:43 +0100

    Added missing module config classes

---
+++ plugins/org.eclipse.epsilon.epl.dt/src/org/eclipse/epsilon/epl/dt/launching/EplModuleConfiguration.java
@@ -0,0 +1,8 @@
+package org.eclipse.epsilon.epl.dt.launching;
+
+import org.eclipse.epsilon.common.dt.launching.tabs.AbstractModuleConfiguration;
+
+public class EplModuleConfiguration extends AbstractModuleConfiguration {
+
+
+}
