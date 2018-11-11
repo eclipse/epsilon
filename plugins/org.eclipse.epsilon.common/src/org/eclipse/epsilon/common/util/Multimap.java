9193156d52920a61ce8c125a8e33d69ffa997ead
Sina Madani <sinadoom@googlemail.com>
2018-11-11 22:21:45 +0000

    Made keys for Multimap thread-safe if the underlying map is thread-safe. Removed unnecessary lookup in put if key is not present. Added hashCode and equals to Multimap.

--- plugins/org.eclipse.epsilon.common/src/org/eclipse/epsilon/common/util/Multimap.java
+++ plugins/org.eclipse.epsilon.common/src/org/eclipse/epsilon/common/util/Multimap.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 The University of York.
+ * Copyright (c) 2010-2018 The University of York.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -11,22 +11,37 @@
 package org.eclipse.epsilon.common.util;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import org.eclipse.epsilon.common.concurrent.ConcurrencyUtils;
 
 public class Multimap<K, V> {
 	
 	protected final Map<K, Collection<V>> storage;
+	protected final boolean isConcurrent;
 	
 	public Multimap() {
 		this(false);
 	}
 	
+	/**
+	 * 
+	 * @param concurrent Whether this implementation should be thread-safe.
+	 * @since 1.6
+	 */
 	public Multimap(boolean concurrent) {
-		storage = concurrent ? ConcurrencyUtils.concurrentMap() : new HashMap<>();
+		storage = (this.isConcurrent = concurrent) == true ?
+			ConcurrencyUtils.concurrentMap() : new HashMap<>();
 	}
 	
+	/**
+	 * 
+	 * @param concurrent Whether this implementation should be thread-safe.
+	 * @param other The Multimap to copy from.
+	 * @since 1.6
+	 */
 	public Multimap(boolean concurrent, Multimap<K, V> other) {
-		storage = concurrent ? ConcurrencyUtils.concurrentMap(other.storage) : new HashMap<>(other.storage);
+		storage = (this.isConcurrent = concurrent) == true ?
+			ConcurrencyUtils.concurrentMap(other.storage) : new HashMap<>(other.storage);
 	}
 	
 	public Collection<V> get(K key) {
@@ -34,11 +49,13 @@ public class Multimap<K, V> {
 	}
 
 	public void put(K key, V value) {
-		if (!storage.containsKey(key)) {
-			storage.put(key, new LinkedList<V>());
+		Collection<V> colForKey = storage.get(key);
+		
+		if (colForKey == null) {
+			storage.put(key, colForKey = isConcurrent ? new ConcurrentLinkedQueue<>() : new LinkedList<>());
 		}
 		
-		storage.get(key).add(value);
+		colForKey.add(value);
 	}
 
 	public boolean remove(K key, V value) {
@@ -68,6 +85,38 @@ public class Multimap<K, V> {
 	
 	public void putAll(K key, Collection<V> values) {
 		storage.put(key, values);
+	}
+	
+	/**
+	 * 
+	 * @return
+	 * @since 1.6
+	 */
+	public boolean isThreadSafe() {
+		return isConcurrent;
+	}
+	
+	/**
+	 * @since 1.6
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (!(obj instanceof Multimap)) return false;
+		
+		Multimap<?, ?> other = (Multimap<?, ?>) obj;
+		
+		return
+			this.isConcurrent == other.isConcurrent &&
+			Objects.equals(this.storage, other.storage);
+	}
+	
+	/**
+	 * @since 1.6
+	 */
+	@Override
+	public int hashCode() {
+		return Objects.hash(isConcurrent, storage);
 	}
 	
 	@Override
