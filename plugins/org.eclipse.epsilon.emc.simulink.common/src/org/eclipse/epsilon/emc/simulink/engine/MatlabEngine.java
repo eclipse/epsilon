4f7d3eddcad3dcfa8dc651cfb38763dd5f2688a6
Betty <basp91@gmail.com>
2020-02-25 18:59:42 +0000

    updated try catch for simulink

--- plugins/org.eclipse.epsilon.emc.simulink.common/src/org/eclipse/epsilon/emc/simulink/engine/MatlabEngine.java
+++ plugins/org.eclipse.epsilon.emc.simulink.common/src/org/eclipse/epsilon/emc/simulink/engine/MatlabEngine.java
@@ -243,7 +243,8 @@ public class MatlabEngine {
 			}
 		}
 		try {
-			cmd = "try\n" + cmd + "\n catch ME \n e = MException(ME.Identifier,ME.msgtextend); e.addCause(ME.cause); throw e; \n end";
+			cmd = "try\n" + cmd + "\n catch ME \n e = MException(ME.identifier,ME.message); throw(e); \n end";
+			//cmd = "try\n" + cmd + "\n catch ME \n ME.identifier\n\n end";
 			evalMethod.invoke(engine, cmd);
 		} catch (InvocationTargetException e) {
 			throw new MatlabException(e);
