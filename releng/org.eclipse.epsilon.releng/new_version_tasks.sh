df1806a9022508e5648010517e5545b36b96ff7d
Sina Madani <sinadoom@googlemail.com>
2020-05-29 22:26:32 +0100

    Improved new_version_tasks.sh

--- releng/org.eclipse.epsilon.releng/new_version_tasks.sh
+++ releng/org.eclipse.epsilon.releng/new_version_tasks.sh
@@ -1,24 +1,22 @@
 set -u
 
+Downloads=/home/data/httpd/download.eclipse.org/epsilon
+Archives=/home/data/httpd/archive.eclipse.org/epsilon
+
 NewVersion=2.1
-DownloadSite=/home/data/httpd/download.eclipse.org/epsilon
-ArchiveSite=/home/data/httpd/archive.eclipse.org/epsilon
+OldVersion=2.0
 
-echo "downloads before: " && ls $DownloadSite | xargs
-echo "archives before: " && ls $ArchiveSite | xargs
+echo "downloads before: " && ls $Downloads | xargs
+echo "archives before: " && ls $Archives | xargs
 
-# Update the array with previous release(s)
-declare -a OldVersions=("obsolete" "1.6" "2.0");
-for version in "${OldVersions[@]}"; do
-  echo "Moving $version...";
-  mkdir $ArchiveSite/$version &&
-  mv $DownloadSite/updates/$version/* $ArchiveSite/$version &&
-  rm -rf $DownloadSite/updates/$version &&
-  mv $DownloadSite/$version/* $ArchiveSite/$version &&
-  rm -rf $DownloadSite/$version
-done
+echo "Moving $OldVersion...";
+mkdir $Archives/$OldVersion &&
+mv $Downloads/updates/$OldVersion/* $Archives/$OldVersion &&
+rm -rf $Downloads/updates/$OldVersion &&
+mv $Downloads/$OldVersion/* $Archives/$OldVersion &&
+rm -rf $Downloads/$OldVersion
 
-cd /home/data/httpd/download.eclipse.org/epsilon &&
+cd $Downloads &&
 mkdir $NewVersion && mkdir updates/$NewVersion &&
 echo "Copying update site" &&
 cp -r interim updates/$NewVersion &&
