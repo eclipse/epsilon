23ef2209fecaf1e4604eded57f13cb3b46ce9c53
Sina Madani <sinadoom@googlemail.com>
2020-05-31 15:10:48 +0100

    Bumped up version to 2.1.0-SNAPSHOT

--- build-and-test.sh
+++ build-and-test.sh
@@ -14,9 +14,9 @@ mvn -f tests/org.eclipse.epsilon.test surefire:test -P unit
 # mvn -f releng verify -P updatesite
 
 # Bump versions (replace X.Y.Z with appropriate numbers)
-#mvn versions:set-property -Dproperty=epsilon.version -DnewVersion=X.Y.Z
-#mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=X.Y.Z
-#mvn -f pom-plain.xml versions:set -DnewVersion=X.Y.Z
+#mvn versions:set-property -Dproperty=epsilon.version -DnewVersion=X.Y.Z-SNAPSHOT
+#mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=X.Y.Z-SNAPSHOT
+#mvn -f pom-plain.xml versions:set -DnewVersion=X.Y.Z-SNAPSHOT
 
 # Query project version
 #mvn help:evaluate -Dexpression=project.version -q -DforceStdout 
