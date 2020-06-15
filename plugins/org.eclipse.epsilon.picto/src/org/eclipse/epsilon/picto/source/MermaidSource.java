aac1e3061f8f32c35841a8af87ddd7fd3979ee37
Sina Madani <sinadoom@googlemail.com>
2020-06-15 13:50:17 +0100

    Migrated Mermaid support from EpsilonLabs

---
+++ plugins/org.eclipse.epsilon.picto/src/org/eclipse/epsilon/picto/source/MermaidSource.java
@@ -0,0 +1,26 @@
+/*********************************************************************
+* Copyright (c) 2020 The University of York.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+**********************************************************************/
+package org.eclipse.epsilon.picto.source;
+
+import org.eclipse.epsilon.picto.source.VerbatimSource;
+
+public class MermaidSource extends VerbatimSource {
+
+	@Override
+	public String getFormat() {
+		return "mermaid";
+	}
+
+	@Override
+	public String getFileExtension() {
+		return "mm";
+	}
+
+}
---
+++ plugins/org.eclipse.epsilon.picto/src/org/eclipse/epsilon/picto/transformers/MermaidContentTransformer.java
@@ -0,0 +1,39 @@
+/*********************************************************************
+* Copyright (c) 2020 The University of York.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+**********************************************************************/
+package org.eclipse.epsilon.picto.transformers;
+
+import org.eclipse.epsilon.picto.PictoView;
+import org.eclipse.epsilon.picto.ViewContent;
+import org.eclipse.epsilon.picto.transformers.ViewContentTransformer;
+
+public class MermaidContentTransformer implements ViewContentTransformer {
+
+	@Override
+	public boolean canTransform(ViewContent content) {
+		return "mermaid".equals(content.getFormat());
+	}
+
+	@Override
+	public String getLabel(ViewContent content) {
+		return "Mermaid";
+	}
+
+	@Override
+	public ViewContent transform(ViewContent content, PictoView pictoView) throws Exception {
+		
+		String html = "<div>" +
+			//"<script src=\"https://cdnjs.cloudflare.com/ajax/libs/mermaid/7.1.2/mermaid.min.js\"></script>\n" + 
+			//"<script>\n" + "mermaid.initialize({startOnLoad:true});\n" + "</script>\n"+
+			"<div class=\"mermaid\">\n" + content.getText() + "</div></div>";
+		
+		return new ViewContent("svg", html, content.getFile(), content.getLayers(), content.getPatches());
+	}
+
+}
