d8dfc68b3ddbf5bb387bab3c74ecac491c3f79c8
Sina Madani <sinadoom@googlemail.com>
2020-06-29 15:32:52 +0100

    Minor fix in new_version_tasks.sh

--- releng/org.eclipse.epsilon.releng/new_version_tasks.sh
+++ releng/org.eclipse.epsilon.releng/new_version_tasks.sh
@@ -5,6 +5,7 @@ Archives=/home/data/httpd/archive.eclipse.org/epsilon
 
 NewVersion=2.1
 OldVersion=2.0
+InterimVersion=interim
 
 echo "downloads before: " && ls $Downloads | xargs
 echo "archives before: " && ls $Archives | xargs
@@ -16,10 +17,10 @@ mv $Downloads/$OldVersion/* $Archives/$OldVersion &&
 rm -rf $Downloads/$OldVersion
 
 cd $Downloads &&
-echo "Copying interim to $NewVersion" &&
-cp -r interim updates/$NewVersion
-if [ -e updates/epsilon-interim-site.zip]; then
-  mv updates/epsilon-interim-site.zip updates/epsilon-${NewVersion}-site.zip
+echo "Copying $InterimVersion to $NewVersion" &&
+cp -r $InterimVersion updates/$NewVersion
+if [ -e updates/epsilon-${InterimVersion}-site.zip]; then
+  mv updates/epsilon-${InterimVersion}-site.zip updates/epsilon-${NewVersion}-site.zip
 fi
 declare -a NewFolders=("javadoc" "jars");
 for folder in "${NewFolders[@]}"; do
@@ -30,8 +31,8 @@ done
 
 ant -f /shared/modeling/tools/promotion/manage-composite.xml add -Dchild.repository=$NewVersion
 
-echo "downloads after: " && ls /home/data/httpd/download.eclipse.org/epsilon | xargs;
-echo "archives after: " && ls /home/data/httpd/archive.eclipse.org/epsilon | xargs;
+echo "downloads after: " && ls $Downloads | xargs;
+echo "archives after: " && ls $Archives | xargs;
 
 #cd /home/data/httpd/download.eclipse.org/epsilon/temp
 #curl -o epsilon-${NewVersion}-signed.zip -F file=@epsilon-${NewVersion}-unsigned.zip http://build.eclipse.org:31338/macsign.php
