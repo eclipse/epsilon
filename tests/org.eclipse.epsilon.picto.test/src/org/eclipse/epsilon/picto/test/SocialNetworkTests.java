b2456058f6f105f7a2f93472475bdef3ec60284c
Dimitris Kolovos <dkolovos@cs.york.ac.uk>
2020-06-30 08:40:56 +0100

    Added a test for Picto layers

--- tests/org.eclipse.epsilon.picto.test/src/org/eclipse/epsilon/picto/test/SocialNetworkTests.java
+++ tests/org.eclipse.epsilon.picto.test/src/org/eclipse/epsilon/picto/test/SocialNetworkTests.java
@@ -1,9 +1,11 @@
 package org.eclipse.epsilon.picto.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import org.eclipse.epsilon.picto.Layer;
 import org.eclipse.epsilon.picto.PictoView;
 import org.eclipse.epsilon.picto.ViewContent;
 import org.eclipse.epsilon.picto.ViewTree;
@@ -21,12 +23,24 @@ public class SocialNetworkTests extends PictoTests {
 		ViewTree modelViewTree = viewTree.getChildren().get(0);
 		assertEquals("Model", modelViewTree.getName());
 		
-		ViewTree socialNetworkViewTree = modelViewTree.getChildren().get(0);
+		ViewTree classesViewTree = modelViewTree.getChildren().get(1);
+		assertEquals("Classes", classesViewTree.getName());
+		
+		ViewTree socialNetworkViewTree = classesViewTree.getChildren().get(1);
 		assertEquals("SocialNetwork", socialNetworkViewTree.getName());
 		
 		ViewContent dotContent = socialNetworkViewTree.getContent();
 		assertEquals("graphviz-dot", dotContent.getFormat());
 		assertTrue(dotContent.getText().contains("digraph"));
+		// By default the referenceLabels layer is turned on
+		assertTrue(dotContent.getText().contains("people[*]"));
+		
+		// Turn the referenceLabels layer off
+		Layer referenceLabelsLayer = socialNetworkViewTree.getContent().getLayers().stream().filter(l -> l.getId().equals("referenceLabels")).findFirst().orElse(null);
+		referenceLabelsLayer.setActive(false);
+		socialNetworkViewTree.clearCache();
+		// Check that there's no people[*] label anywhere in the generated dot
+		assertFalse(socialNetworkViewTree.getContent().getText().contains("people[*]"));
 		
 		ViewContent svgContent = dotContent.getNext(pictoView);
 		assertEquals("svg", svgContent.getFormat());
@@ -39,9 +53,9 @@ public class SocialNetworkTests extends PictoTests {
 		ViewContent htmlWithZoomContent = htmlContent.getNext(pictoView);
 		assertEquals("html", htmlWithZoomContent.getFormat());
 		assertTrue(htmlWithZoomContent.getText().contains("picto-zoom"));
+		assertTrue(htmlWithZoomContent.getText().contains("people[*]"));
 		
 		assertNull(htmlWithZoomContent.getNext(pictoView));
-		
 		
 	}
 	
