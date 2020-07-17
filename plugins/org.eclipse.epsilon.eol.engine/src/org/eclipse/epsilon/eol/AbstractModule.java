42869e34474e2c6f22aeb6fdd2a467d5da6fab83
Dimitris Kolovos <dkolovos@cs.york.ac.uk>
2020-07-17 18:53:51 +0100

    Reverted last commit

--- plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/AbstractModule.java
+++ plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/AbstractModule.java
@@ -141,7 +141,18 @@ public abstract class AbstractModule extends AbstractModuleElement implements IM
 		List<CommonToken> comments = new ArrayList<>();
 		
 		if (stream.getTokens().isEmpty()) {
-			stream.fill();
+			try {
+				Method fill = stream.getClass().getMethod("fill");
+				if (fill != null) try {
+					fill.invoke(stream);
+				}
+				catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+					throw new IllegalStateException(ex);
+				}
+			}
+			catch (NoSuchMethodException | SecurityException ex) {
+				// ANTLR 3.2 probably
+			}
 		}
 		
 		for (Object t : stream.getTokens()) {
