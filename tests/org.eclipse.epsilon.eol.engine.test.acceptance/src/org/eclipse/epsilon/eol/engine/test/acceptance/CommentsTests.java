6a7e83c54d8edb10614b75360e16aae670688f0b
Sina Madani <sinadoom@googlemail.com>
2020-07-17 12:31:15 +0100

    Added missing copyright header
    Signed-off-by: Sina Madani <sinadoom@googlemail.com>

--- tests/org.eclipse.epsilon.eol.engine.test.acceptance/src/org/eclipse/epsilon/eol/engine/test/acceptance/CommentsTests.java
+++ tests/org.eclipse.epsilon.eol.engine.test.acceptance/src/org/eclipse/epsilon/eol/engine/test/acceptance/CommentsTests.java
@@ -1,10 +1,23 @@
+/*********************************************************************
+ * Copyright (c) 2020 The University of York.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+**********************************************************************/
 package org.eclipse.epsilon.eol.engine.test.acceptance;
 
 import static org.junit.Assert.assertEquals;
-
 import org.eclipse.epsilon.eol.EolModule;
 import org.junit.Test;
 
+/**
+ * 
+ * @author Dimitris Kolovos
+ * @since 2.2
+ */
 public class CommentsTests {
 	
 	@Test
