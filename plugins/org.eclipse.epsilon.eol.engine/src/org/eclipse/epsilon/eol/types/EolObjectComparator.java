be6ed2b531b6ad90c3c185f009ed21fd82e499ff
Dimitris Kolovos <dkolovos@cs.york.ac.uk>
2020-11-23 09:44:51 +0000

    Fixed equality checking for bags

--- plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/types/EolObjectComparator.java
+++ plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/types/EolObjectComparator.java
@@ -9,8 +9,20 @@
  ******************************************************************************/
 package org.eclipse.epsilon.eol.types;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class EolObjectComparator {
+	
+	protected static Comparator<Object> comparator = new Comparator<Object>() {
+		@Override
+		public int compare(Object o1, Object o2) {
+			return String.valueOf(o1).compareTo(String.valueOf(o2));
+		}
+	};
 	
 	public static boolean equals(Object o1, Object o2) {
 		if (o1 == null && o2 == null) return true;
@@ -19,31 +31,22 @@ public class EolObjectComparator {
 		if (o1 instanceof Number && o2 instanceof Number) {
 			return NumberUtil.isEqual((Number)o1, (Number)o2);
 		}
-
-		/*
+		
 		if (o1 instanceof Collection && o2 instanceof Collection) {
-			Collection c1 = (Collection) o1;
-			Collection c2 = (Collection) o2;
-			
-			if (!EolCollectionType.getTypeName(c1).equals(EolCollectionType.getTypeName(c2))) return false;
-			boolean isOrdered = EolCollectionType.isOrdered(c1);
-			
-			if (c1.size() != c2.size()) { return false; } 
-			else {
-				Iterator it1 = c1.iterator();
-				Iterator it2 = c2.iterator();
-				while (it1.hasNext()) {
-					Object next = it1.next();
-					if (isOrdered) {
-						if (!equals(next, it2.next())) return false;
-					}
-					else {
-						if (!c2.contains(next)) return false;
-					}
-				}
-				return true;
+			Collection<?> c1 = (Collection<?>) o1;
+			Collection<?> c2 = (Collection<?>) o2;
+		
+			if (EolCollectionType.Bag.isType(c1) && EolCollectionType.Bag.isType(c2)) {
+				List<Object> l1 = new ArrayList<>(c1);
+				List<Object> l2 = new ArrayList<>(c2);
+				
+				Collections.sort(l1, comparator);
+				Collections.sort(l2, comparator);
+				
+				return l1.equals(l2);
 			}
-		}*/
+		
+		}
 		
 		return o1.equals(o2);
 	}
