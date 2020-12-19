8cd22dd8486fd1a877fd550355b897dca897e513
Dimitris Kolovos <dkolovos@cs.york.ac.uk>
2020-12-19 09:15:32 +0000

    Added documentation to RegisterTaskStandalome
    Signed-off-by: Dimitris Kolovos <dkolovos@cs.york.ac.uk>

--- plugins/org.eclipse.epsilon.workflow.emf/ant/org/eclipse/epsilon/workflow/tasks/emf/RegisterTaskStandalone.java
+++ plugins/org.eclipse.epsilon.workflow.emf/ant/org/eclipse/epsilon/workflow/tasks/emf/RegisterTaskStandalone.java
@@ -1,3 +1,12 @@
+/*********************************************************************
+* Copyright (c) 2020 The University of York.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+**********************************************************************/
 package org.eclipse.epsilon.workflow.tasks.emf;
 
 import java.io.File;
@@ -7,6 +16,13 @@ import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.epsilon.emc.emf.EmfUtil;
 import org.eclipse.epsilon.workflow.tasks.EpsilonTask;
 
+/**
+ * Standalone implementation of the epsilon.emf.register
+ * ANT task that does not support permanent metamodel
+ * registration and does not depend on emf.dt.
+ * We use this class in tasks.xml and the other 
+ * RegisterTask class from the src folder in plugin.xml
+ */
 public class RegisterTaskStandalone extends EpsilonTask {
 	
 	protected File file;
@@ -21,7 +37,7 @@ public class RegisterTaskStandalone extends EpsilonTask {
 		try {
 			EmfUtil.register(file, EPackage.Registry.INSTANCE);
 			if (permanently) {
-			    warn("permanent is not supported in standalone mode");
+			    warn("Permanent registration is not supported in the standalone mode");
 			}
 		}
 		catch (Exception e) {
