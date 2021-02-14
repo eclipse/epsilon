f014c8d22512901e12f39a11f8d8fb7db7c46b3b
Dimitris Kolovos <dkolovos@cs.york.ac.uk>
2021-02-14 17:05:28 +0000

    Added support for Emfatic metamodels to standalone ANT tasks

--- plugins/org.eclipse.epsilon.workflow.emf/ant/org/eclipse/epsilon/workflow/tasks/emf/ResourceFactoryRegistryManager.java
+++ plugins/org.eclipse.epsilon.workflow.emf/ant/org/eclipse/epsilon/workflow/tasks/emf/ResourceFactoryRegistryManager.java
@@ -1,6 +1,7 @@
 package org.eclipse.epsilon.workflow.tasks.emf;
 
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.Resource.Factory;
 import org.eclipse.emf.ecore.resource.Resource.Factory.Registry;
 import org.eclipse.epsilon.flexmi.FlexmiResourceFactory;
 
@@ -10,6 +11,16 @@ public class ResourceFactoryRegistryManager {
 		Registry resourceFactoryRegistry = Resource.Factory.Registry.INSTANCE;
 		if (!resourceFactoryRegistry.getExtensionToFactoryMap().containsKey("flexmi")) {
 			resourceFactoryRegistry.getExtensionToFactoryMap().put("flexmi", new FlexmiResourceFactory());
+		}
+		if (!resourceFactoryRegistry.getExtensionToFactoryMap().containsKey("emf")) {
+			try {
+				Resource.Factory emfaticResourceFactory = 
+						(Factory) Class.forName("org.eclipse.emf.emfatic.core.EmfaticResourceFactory").newInstance();
+				resourceFactoryRegistry.getExtensionToFactoryMap().put("emf", emfaticResourceFactory);
+			}
+			catch (Exception ex) {
+				// Ignore exception
+			}
 		}
 	}
 	
