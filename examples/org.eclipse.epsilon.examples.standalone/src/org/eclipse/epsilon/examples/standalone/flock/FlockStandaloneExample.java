d2aca1ec983910379842fe4cca67329bf948911e
Dimitris Kolovos <dkolovos@cs.york.ac.uk>
2021-05-03 12:58:42 +0100

    Added Flock standalone example

---
+++ examples/org.eclipse.epsilon.examples.standalone/src/org/eclipse/epsilon/examples/standalone/flock/FlockStandaloneExample.java
@@ -0,0 +1,48 @@
+package org.eclipse.epsilon.examples.standalone.flock;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.eclipse.epsilon.emc.emf.EmfModel;
+import org.eclipse.epsilon.flock.FlockModule;
+
+public class FlockStandaloneExample {
+	
+	public static void main(String[] args) throws Exception {
+		
+		Path root = Paths.get(FlockStandaloneExample.class.getResource("").toURI()),
+				modelsRoot = root.getParent().resolve("models");
+			
+		// Set up the original model
+		EmfModel original = new EmfModel();
+		original.setName("Source");
+		original.setReadOnLoad(true);
+		original.setStoredOnDisposal(false);
+		original.setMetamodelFile(modelsRoot.resolve("Tree.ecore").toAbsolutePath().toString());
+		original.setModelFile(modelsRoot.resolve("Tree.xmi").toAbsolutePath().toString());
+		original.load();
+		
+		// Set up the migrated model
+		EmfModel migrated = new EmfModel();
+		migrated.setName("Migrated");
+		migrated.setReadOnLoad(false);
+		migrated.setStoredOnDisposal(true);
+		migrated.setMetamodelFile(modelsRoot.resolve("Tree.ecore").toAbsolutePath().toString());
+		migrated.setModelFile(modelsRoot.resolve("Tree.migrated.xmi").toAbsolutePath().toString().replace("/target/classes/", "/src/"));
+		migrated.load();
+		
+		System.out.println(migrated.getModelFile());
+		
+		FlockModule module = new FlockModule();
+		module.parse(root.resolve("tree2tree.mig"));
+		module.getContext().getModelRepository().addModel(original);
+		module.getContext().getModelRepository().addModel(migrated);
+		module.getContext().setOriginalModel(original);
+		module.getContext().setMigratedModel(migrated);
+		module.execute();
+		
+		// Save the migrated model
+		module.getContext().getModelRepository().dispose();
+	}
+	
+}
