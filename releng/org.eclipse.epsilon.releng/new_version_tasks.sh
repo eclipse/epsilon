3b2733ec1ef8376bc796ffd3fd6ad3a4b049c4ac
Sina Madani <sinadoom@googlemail.com>
2021-05-15 13:03:58 +0100

    Use stable instead of version name for Javadocs

--- releng/org.eclipse.epsilon.releng/new_version_tasks.sh
+++ releng/org.eclipse.epsilon.releng/new_version_tasks.sh
@@ -4,16 +4,18 @@ Downloads=/home/data/httpd/download.eclipse.org/epsilon
 Archives=/home/data/httpd/archive.eclipse.org/epsilon
 UpdatesName=updates
 JavadocsName=javadoc
+StableName=stable
 InterimVersion=interim
-InterimJavadocs=$InterimVersion-$JavadocsName
 NewVersion=2.4
 OldVersion=2.3
+InterimJavadocs=$InterimVersion-$JavadocsName
+Javadocs=$StableName/$JavadocsName
 
 echo "Moving $OldVersion..." &&
 mkdir -p $Archives/$OldVersion &&
 cp -r $Downloads/$UpdatesName/$OldVersion/* $Archives/$OldVersion &&
-mv $Downloads/$OldVersion/* $Archives/$OldVersion &&
-rm -rf $Downloads/$OldVersion &&
+mv $Downloads/$Stable/* $Archives/$OldVersion &&
+rm -rf $Downloads/$Javadocs &&
 cd $Downloads &&
 echo "Copying update site..." &&
 mkdir -p $UpdatesName/$NewVersion &&
@@ -23,9 +25,9 @@ if [ -e $UpdatesName/$NewVersion/epsilon-${InterimVersion}-site.zip ]; then
   mv $UpdatesName/$NewVersion/epsilon-${InterimVersion}-site.zip $UpdatesName/$NewVersion/epsilon-${NewVersion}-site.zip
 fi &&
 echo "Copying $JavadocsName..." &&
-mkdir -p $NewVersion/$JavadocsName &&
-cp -r $InterimJavadocs/* $NewVersion/$JavadocsName &&
-rm -rf $NewVersion/$JavadocsName/$InterimJavadocs &&
+mkdir -p $Javadocs &&
+cp -r $InterimJavadocs/* $Javadocs &&
+rm -rf $Javadocs/$InterimJavadocs &&
 echo "Adding $NewVersion to composite..." &&
 cd $Downloads/$UpdatesName &&
 ant -f /shared/modeling/tools/promotion/manage-composite.xml add -Dchild.repository=$NewVersion
