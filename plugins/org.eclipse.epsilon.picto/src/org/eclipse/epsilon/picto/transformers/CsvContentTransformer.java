53db7c7f45b5f3d9b04fa2a63fc2f554ce506ec7
alfonsodelavega <alfonso.delavega@tuta.io>
2021-09-25 16:10:34 +0100

    Picto: fix number sorting in CSVContentTransformer
    Future: allow setting the default column sorting order

--- plugins/org.eclipse.epsilon.picto/src/org/eclipse/epsilon/picto/transformers/CsvContentTransformer.java
+++ plugins/org.eclipse.epsilon.picto/src/org/eclipse/epsilon/picto/transformers/CsvContentTransformer.java
@@ -10,6 +10,8 @@
 package org.eclipse.epsilon.picto.transformers;
 
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
@@ -28,19 +30,24 @@ public class CsvContentTransformer implements ViewContentTransformer {
 	public ViewContent transform(ViewContent content, PictoView pictoView) throws Exception {
 
 		CSVFormat format = CSVFormat.RFC4180.withFirstRecordAsHeader();
-		CSVParser records = format.parse(new StringReader(content.getText()));
+		CSVParser parser = format.parse(new StringReader(content.getText()));
+
+		List<CSVRecord> records = parser.getRecords();
+		List<String> columnTypes = getColumnTypes(records);
 
 		StringBuilder result = new StringBuilder();
 		result.append(
 				"<table class=\"table striped table-border row-hover\" data-horizontal-scroll=\"true\" data-role=\"table\" data-pagination=\"true\">")
 				.append("<thead>");
-		for (String column : records.getHeaderNames()) {
-			result.append("<th class=\"sortable-column sort-asc\">")
-					.append(column)
+
+		for (int i = 0; i < parser.getHeaderNames().size(); i++) {
+			result.append("<th data-sortable=\"true\" data-sort-dir=\"asc\"")
+					.append(" data-format=\"" + getColumnType(columnTypes, i) + "\">")
+					.append(parser.getHeaderNames().get(i))
 					.append("</th>");
 		}
-		result.append("</thead>")
-				.append("<tbody>");
+		result.append("</thead>").append("<tbody>");
+
 		for (CSVRecord record : records) {
 			result.append("<tr>");
 			for (String cell : record) {
@@ -48,12 +55,68 @@ public class CsvContentTransformer implements ViewContentTransformer {
 			}
 			result.append("</tr>");
 		}
-		result.append("</tbody>")
-				.append("</table>");
+		result.append("</tbody>").append("</table>");
 		
 		return new ViewContent("html", pictoView.getViewRenderer().getHtml(result.toString()), content);
 	}
 	
+	protected String getColumnType(List<String> columnTypes, int columnIndex) {
+		if (columnTypes != null) {
+			return columnTypes.get(columnIndex);
+		}
+		return "string";
+	}
+
+	protected String getCellType(String cell) {
+		try {
+			Double.parseDouble(cell);
+			return "number";
+		}
+		catch (NumberFormatException n) {
+		}
+		return "string";
+	}
+
+	public List<String> getColumnTypes(List<CSVRecord> records) {
+		if (records.size() == 0) {
+			return null;
+		}
+		List<String> columnTypes = new ArrayList<String>();
+		// check type over first rows
+		int rowsToCheck = 5;
+		for (int row = 0; row < rowsToCheck && row < records.size(); row++) {
+			CSVRecord record = records.get(row);
+			int column = 0;
+			for (String cell : record) {
+				String cellType = getCellType(cell);
+				if (row > 0) {
+					if (!cellType.equals(columnTypes.get(column))) {
+						// default to string
+						columnTypes.set(column, "string");
+					}
+				}
+				else {
+					columnTypes.add(cellType);
+				}
+				column++;
+			}
+		}
+		return columnTypes;
+	}
+
+	public boolean isNumber(String s) {
+		if (s == null) {
+			return false;
+		}
+		try {
+			Double.parseDouble(s);
+		}
+		catch (NumberFormatException nfe) {
+			return false;
+		}
+		return true;
+	}
+
 	@Override
 	public String getLabel(ViewContent content) {
 		return "CSV";
