25e0205256845e98e338f738f1b2e90710537de0
Sina Madani <sinadoom@googlemail.com>
2021-12-06 18:19:06 +0000

    Fixed AppendTests on Windows
    Signed-off-by: Sina Madani <sinadoom@googlemail.com>

--- tests/org.eclipse.epsilon.egx.engine.test.acceptance/src/org/eclipse/epsilon/egx/engine/test/acceptance/hutn/AppendTests.java
+++ tests/org.eclipse.epsilon.egx.engine.test.acceptance/src/org/eclipse/epsilon/egx/engine/test/acceptance/hutn/AppendTests.java
@@ -11,10 +11,11 @@ package org.eclipse.epsilon.egx.engine.test.acceptance.hutn;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.File;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.eclipse.epsilon.egl.EgxModule;
 import org.junit.Test;
@@ -23,13 +24,13 @@ public class AppendTests {
 	
 	@Test
 	public void testAppendWithAnnotation() throws Exception {
-		String egx = "@append\n"       +
+		String egx = "@append" + System.lineSeparator()       +
 				 "rule I2T transform i : Integer in: 1.to(2) {"    +
 		         "    template: _template " +
 		         "    target: 'out.txt' "    + 
 		         "}";
 		
-		testAppend(egx, "1\n2");	
+		testAppend(egx, "1" + System.lineSeparator() + "2");	
 	}
 	
 	@Test
@@ -40,7 +41,7 @@ public class AppendTests {
 		             "    target: 'out.txt' "    + 
 		             "}";
 		
-		testAppend(egx, "1\n2");	
+		testAppend(egx, "1" + System.lineSeparator() + "2");	
 	}
 	
 	@Test
@@ -67,22 +68,23 @@ public class AppendTests {
 	public void testAppend(String egx, String expected) throws Exception {
 		
 		String egl = "[%=i%]";
-		File template = toFile(egl);
+		URI template = toURI(egl);
 		Path outputRoot = Files.createTempDirectory(null);
 		
 		EgxModule module = new EgxModule(outputRoot);
 		module.parse(egx);
-		module.getContext().getFrameStack().put("_template", template.getAbsolutePath());
+		module.getContext().getFrameStack().put("_template", template);
 		module.execute();
 		
-		assertEquals(expected, new String(Files.readAllBytes(new File(outputRoot.toFile(), "out.txt").toPath())));
+		String actual = new String(Files.readAllBytes(Paths.get(outputRoot.toString(), "out.txt")));
+		assertEquals(expected, actual);
 		
 	}
 	
-	public File toFile(String content) throws Exception {
-		Path path = Files.createTempFile(null, "egl");
+	public URI toURI(String content) throws Exception {
+		Path path = Files.createTempFile(null, ".egl");
 		Files.write(path, content.getBytes(StandardCharsets.UTF_8));
-		return path.toFile();
+		return path.toAbsolutePath().toUri();
 
 	}
 	
