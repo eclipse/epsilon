cb15a240256927dc9ed89a3c0af185435bc465f4
Dimitris Kolovos <dkolovos@cs.york.ac.uk>
2022-03-08 22:39:26 +0000

    Improved fix for outdentation in EGX

--- plugins/org.eclipse.epsilon.egl.engine/src/org/eclipse/epsilon/egl/execute/operations/OutdentOperation.java
+++ plugins/org.eclipse.epsilon.egl.engine/src/org/eclipse/epsilon/egl/execute/operations/OutdentOperation.java
@@ -1,5 +1,6 @@
 package org.eclipse.epsilon.egl.execute.operations;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -13,11 +14,19 @@ import org.eclipse.epsilon.eol.execute.operations.simple.SimpleOperation;
 
 public class OutdentOperation extends SimpleOperation {
 	
-	protected Set<String> ids = new HashSet<>(); 
+	protected Set<String> ids = new HashSet<>();
+	// Execution listeners for indenting the output at the end 
+	// of the block containing the call to the _outdent function
+	protected List<IExecutionListener> executionListenersForRemoval = new ArrayList<>();
 	
 	@Override
 	public Object execute(Object source, List<?> parameters, IEolContext context, ModuleElement ast)
 			throws EolRuntimeException {
+		
+		// Remove any execution listeners marked for removal
+		for (IExecutionListener executionListener : executionListenersForRemoval) {
+			context.getExecutorFactory().removeExecutionListener(executionListener);
+		}
 		
 		String id = String.valueOf(parameters.get(0));
 		
@@ -29,23 +38,17 @@ public class OutdentOperation extends SimpleOperation {
 			
 			context.getExecutorFactory().addExecutionListener(new IExecutionListener() {
 				
-				// Ideally we should remove the execution listener 
-				// after its first successful execution but as this would cause a 
-				// concurrent modification exception, we deactivate it instead
-				boolean active = true;
-				
 				@Override
 				public void finishedExecutingWithException(ModuleElement ast, EolRuntimeException exception, IEolContext context) {}
 				
 				@Override
 				public void finishedExecuting(ModuleElement ast, Object result, IEolContext context) {
-				
-					if (!active) return;
 					
 					if (ast == moduleElement && ids.contains(id)) {
 						buffer.getOutdentationFormatter().indent(buffer.getOffset());
 						ids.remove(id);
-						active = false;
+						// Mark the execution listener for removal
+						executionListenersForRemoval.add(this);
 					}
 				}
 				
