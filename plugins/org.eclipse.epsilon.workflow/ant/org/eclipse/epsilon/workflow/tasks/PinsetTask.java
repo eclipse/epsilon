5a8acf9e955e6ad113b4f63a344616008016cc02
alfonsodelavega <alfonso.delavega@unican.es>
2023-03-29 11:43:11 +0200

    Add pinset output folder property wrt the ant project

--- plugins/org.eclipse.epsilon.workflow/ant/org/eclipse/epsilon/workflow/tasks/PinsetTask.java
+++ plugins/org.eclipse.epsilon.workflow/ant/org/eclipse/epsilon/workflow/tasks/PinsetTask.java
@@ -1,9 +1,17 @@
 package org.eclipse.epsilon.workflow.tasks;
 
+import java.io.File;
+import java.io.FileWriter;
+
+import org.apache.tools.ant.BuildException;
 import org.eclipse.epsilon.eol.IEolModule;
+import org.eclipse.epsilon.eol.exceptions.models.EolModelLoadingException;
+import org.eclipse.epsilon.eol.exceptions.models.EolModelNotFoundException;
 import org.eclipse.epsilon.pinset.PinsetModule;
 
 public class PinsetTask extends ExecutableModuleTask {
+
+	private String outputFolder;
 
 	@Override
 	protected void initialize() throws Exception {
@@ -18,4 +26,21 @@ public class PinsetTask extends ExecutableModuleTask {
 		return new PinsetModule();
 	}
 
+	@Override
+	protected void configureModule() throws EolModelNotFoundException, BuildException, EolModelLoadingException {
+		super.configureModule();
+
+		if (outputFolder != null) {
+			File baseDir = getProject().getBaseDir();
+			((PinsetModule) module).setOutputFolder(baseDir.getAbsolutePath() + "/" + outputFolder);
+		}
+	}
+
+	public String getOutputFolder() {
+		return outputFolder;
+	}
+
+	public void setOutputFolder(String outputFolder) {
+		this.outputFolder = outputFolder;
+	}
 }
