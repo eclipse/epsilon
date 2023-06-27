e3d6a230dd54e10c3d570cce7732bb827950d904
Antonio García-Domínguez <agarcdomi@gmail.com>
2023-06-28 00:20:12 +0100

    Touch up changed file headers

--- plugins/org.eclipse.epsilon.ecl.engine/src/org/eclipse/epsilon/ecl/dom/MatchRule.java
+++ plugins/org.eclipse.epsilon.ecl.engine/src/org/eclipse/epsilon/ecl/dom/MatchRule.java
@@ -1,11 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2008 The University of York.
+ * Copyright (c) 2008-2023 The University of York.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  * 
  * Contributors:
  *     Dimitrios Kolovos - initial API and implementation
+ *     Antonio Garcia-Dominguez - protect against NPE in toString()
  ******************************************************************************/
 package org.eclipse.epsilon.ecl.dom;
 
--- tests/org.eclipse.epsilon.ecl.engine.test.acceptance/src/org/eclipse/epsilon/ecl/engine/test/acceptance/EclAcceptanceTestSuite.java
+++ tests/org.eclipse.epsilon.ecl.engine.test.acceptance/src/org/eclipse/epsilon/ecl/engine/test/acceptance/EclAcceptanceTestSuite.java
@@ -1,11 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2011 The University of York.
+ * Copyright (c) 2011-2023 The University of York.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  * 
  * Contributors:
  *     Louis Rose - initial API and implementation
+ *     Antonio Garcia-Dominguez - add MatchTraceTest
  ******************************************************************************/
 package org.eclipse.epsilon.ecl.engine.test.acceptance;
 
