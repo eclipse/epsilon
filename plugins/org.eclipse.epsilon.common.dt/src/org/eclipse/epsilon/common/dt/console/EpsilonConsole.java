8953c3b6282dc1f908d3888b95f4733a20f67721
Dimitris Kolovos <dimitris.kolovos@york.ac.uk>
2023-07-03 21:13:50 +0100

    Restored input stream colour.

--- plugins/org.eclipse.epsilon.common.dt/src/org/eclipse/epsilon/common/dt/console/EpsilonConsole.java
+++ plugins/org.eclipse.epsilon.common.dt/src/org/eclipse/epsilon/common/dt/console/EpsilonConsole.java
@@ -123,7 +123,7 @@ public class EpsilonConsole {
 				errorOutputStream.setColor(display.getSystemColor(SWT.COLOR_RED));
 				debugOutputStream.setColor(display.getSystemColor(SWT.COLOR_BLACK));
 				warningOutputStream.setColor(display.getSystemColor(SWT.COLOR_DARK_YELLOW));
-				ioConsole.getInputStream().setColor(display.getSystemColor(SWT.COLOR_DARK_GREEN));
+				ioConsole.getInputStream().setColor(display.getSystemColor(SWT.COLOR_GREEN));
 			}
 		});
 	}
