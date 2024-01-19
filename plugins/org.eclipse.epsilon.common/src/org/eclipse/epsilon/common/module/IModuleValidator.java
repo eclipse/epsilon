f9b82cb42d32a30184f26f06d9492dcb155df272
Panagiotis Kourouklidis <panagiotis.kourouklidis@york.ac.uk>
2024-01-19 11:20:01 +0000

    Repo configuration

--- plugins/org.eclipse.epsilon.common/src/org/eclipse/epsilon/common/module/IModuleValidator.java
+++ plugins/org.eclipse.epsilon.common/src/org/eclipse/epsilon/common/module/IModuleValidator.java
@@ -12,7 +12,7 @@ package org.eclipse.epsilon.common.module;
 import java.util.List;
 
 
-public interface IModuleValidator {
+public interface IModuleValidator{
 	
 	public List<ModuleMarker> validate(IModule module);
 
