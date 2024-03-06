bb8f33701bffc790833ac3d686ccba0cb7a74ab7
Antonio García-Domínguez <agarcdomi@gmail.com>
2024-03-06 10:49:37 +0000

    Use Dash for plain Maven build too

--- list-dependencies.sh
+++ list-dependencies.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Plain Maven build
+mvn -f pom-plain.xml org.eclipse.dash:license-tool-plugin:license-check -Ddash.summary=DEPENDENCIES-PLAIN
+
+# Tycho build
 mvn org.eclipse.dash:license-tool-plugin:license-check -Ddash.summary=DEPENDENCIES
