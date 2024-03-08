5ce08e424d82facb6a32b84ee881954ad6fde3eb
Antonio García-Domínguez <agarcdomi@gmail.com>
2024-03-08 11:42:25 +0000

    Update download-emf-compare.sh to fetch EMF Compare from 2023-12 SR

--- plugins/org.eclipse.epsilon.eunit.cmp.emf/download-emf-compare.sh
+++ plugins/org.eclipse.epsilon.eunit.cmp.emf/download-emf-compare.sh
@@ -3,9 +3,9 @@
 # Downloads EMF Compare and installs it as a local dependency,
 # to be embedded in the plain Maven eunit.cmp.emf JAR.
 
-EMF_COMPARE_VERSION="3.5.3.202111290942"
-EMF_COMPARE_URL="https://download.eclipse.org/releases/2021-12/202112081000/plugins/org.eclipse.emf.compare_${EMF_COMPARE_VERSION}.jar"
-EMF_COMPARE_JAR="org.eclipse.emf.compare_3.5.3.202111290942.jar"
+EMF_COMPARE_VERSION="3.5.3.202311200811"
+EMF_COMPARE_URL="https://download.eclipse.org/releases/2023-12/202312061001/plugins/org.eclipse.emf.compare_${EMF_COMPARE_VERSION}.jar"
+EMF_COMPARE_JAR="org.eclipse.emf.compare_${EMF_COMPARE_VERSION}.jar"
 
 cd "$(dirname "$(readlink -f "$0")")"
 curl -C - -o "$EMF_COMPARE_JAR" -O "$EMF_COMPARE_URL"
@@ -19,4 +19,4 @@ mvn -f pom-plain.xml clean install:install-file \
 
 # Powershell version:
 
- #mvn -f pom-plain.xml install:install-file "-Dfile=org.eclipse.emf.compare_3.5.3.202111290942.jar" "-DgroupId=org.eclipse.emf" "-DartifactId=org.eclipse.emf.compare" "-Dversion=3.5.3.202111290942" "-Dpackaging=jar"
+ #mvn -f pom-plain.xml install:install-file "-Dfile=org.eclipse.emf.compare_3.5.3.202311200811.jar" "-DgroupId=org.eclipse.emf" "-DartifactId=org.eclipse.emf.compare" "-Dversion=3.5.3.202311200811" "-Dpackaging=jar"
