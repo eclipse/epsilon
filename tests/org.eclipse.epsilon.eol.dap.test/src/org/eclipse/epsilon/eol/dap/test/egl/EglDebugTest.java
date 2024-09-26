c7da8abf9618a48173b48705010a123b3613fc32
Antonio Garcia-Dominguez <agarcdomi@gmail.com>
2024-09-26 14:59:05 +0200

    [EGL DAP] Add tests for column start/end ranges

--- tests/org.eclipse.epsilon.eol.dap.test/src/org/eclipse/epsilon/eol/dap/test/egl/EglDebugTest.java
+++ tests/org.eclipse.epsilon.eol.dap.test/src/org/eclipse/epsilon/eol/dap/test/egl/EglDebugTest.java
@@ -20,6 +20,7 @@ import org.eclipse.epsilon.egl.EglModule;
 import org.eclipse.epsilon.eol.dap.test.AbstractEpsilonDebugAdapterTest;
 import org.eclipse.lsp4j.debug.ContinueArguments;
 import org.eclipse.lsp4j.debug.SetBreakpointsResponse;
+import org.eclipse.lsp4j.debug.StackTraceResponse;
 import org.eclipse.lsp4j.debug.StoppedEventArgumentsReason;
 import org.eclipse.lsp4j.debug.Variable;
 import org.junit.Test;
@@ -58,14 +59,42 @@ public class EglDebugTest extends AbstractEpsilonDebugAdapterTest {
 		// Break on the first static segment of line 2
 		assertStoppedBecauseOf(StoppedEventArgumentsReason.BREAKPOINT);
 
+		StackTraceResponse stackTrace = getStackTrace();
+		assertEquals("Should be stopped at the second line", 2, getCurrentLine(stackTrace));
+		assertEquals("Should be stopped from the first column", 1, getCurrentStartColumn(stackTrace));
+		assertEquals("Should be stopped until column 7", 7, getCurrentEndColumn(stackTrace));
+
 		Map<String, Variable> topVariables = getVariablesFromTopStackFrame();
 		Variable numbersVariable = topVariables.get("name");
 		assertNotNull("The top scope should have a 'name' variable", numbersVariable);
 		assertEquals("String", numbersVariable.getType());
 		assertEquals("Joe", numbersVariable.getValue());
 
-		// Remove breakpoints and continue
-		adapter.setBreakpoints(createBreakpoints()).get();
+		// Continue: it should stop in the dynamic region
+		adapter.continue_(new ContinueArguments()).get();
+		assertStoppedBecauseOf(StoppedEventArgumentsReason.BREAKPOINT);
+		stackTrace = getStackTrace();
+		assertEquals("Should be stopped at the second line", 2, getCurrentLine(stackTrace));
+		assertEquals("Should be stopped from column 10", 10, getCurrentStartColumn(stackTrace));
+		assertEquals("Should be stopped until column 14", 14, getCurrentEndColumn(stackTrace));
+
+		// Continue: it should stop in the trailing static region
+		adapter.continue_(new ContinueArguments()).get();
+		assertStoppedBecauseOf(StoppedEventArgumentsReason.BREAKPOINT);
+		stackTrace = getStackTrace();
+		assertEquals("Should be stopped at the second line", 2, getCurrentLine(stackTrace));
+		assertEquals("Should be stopped from column 16", 16, getCurrentStartColumn(stackTrace));
+		assertEquals("Should be stopped until column 17", 17, getCurrentEndColumn(stackTrace));
+
+		// Continue: it should stop in the newline part
+		adapter.continue_(new ContinueArguments()).get();
+		assertStoppedBecauseOf(StoppedEventArgumentsReason.BREAKPOINT);
+		stackTrace = getStackTrace();
+		assertEquals("Should be stopped at the second line", 2, getCurrentLine(stackTrace));
+		assertEquals("Should be stopped at column 17", 17, getCurrentStartColumn(stackTrace));
+		assertEquals("Should be stopped until column 17", 17, getCurrentEndColumn(stackTrace));
+
+		// Continue: it should complete
 		adapter.continue_(new ContinueArguments()).get();
 		assertProgramCompletedSuccessfully();
 	}
