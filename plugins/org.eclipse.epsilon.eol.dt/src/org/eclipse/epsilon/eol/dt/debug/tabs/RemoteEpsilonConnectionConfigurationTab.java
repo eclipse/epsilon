40157b0773440aeae4f2d2dbed79363a819b38be
Antonio Garcia-Dominguez <agarcdomi@gmail.com>
2024-10-21 14:09:45 -0700

    Add option to Remote Epsilon Application launch configuration dialog

--- plugins/org.eclipse.epsilon.eol.dt/src/org/eclipse/epsilon/eol/dt/debug/tabs/RemoteEpsilonConnectionConfigurationTab.java
+++ plugins/org.eclipse.epsilon.eol.dt/src/org/eclipse/epsilon/eol/dt/debug/tabs/RemoteEpsilonConnectionConfigurationTab.java
@@ -9,33 +9,51 @@
  *******************************************************************************/
 package org.eclipse.epsilon.eol.dt.debug.tabs;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
 import org.eclipse.epsilon.common.dt.util.LogUtil;
+import org.eclipse.epsilon.eol.dap.EpsilonDebugAdapter;
 import org.eclipse.epsilon.eol.dt.EolPlugin;
 import org.eclipse.lsp4e.debug.DSPPlugin;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
 @SuppressWarnings("restriction")
-public class RemoteEpsilonConnectionConfigurationTab extends AbstractLaunchConfigurationTab implements ModifyListener {
+public class RemoteEpsilonConnectionConfigurationTab extends AbstractLaunchConfigurationTab implements ModifyListener, SelectionListener {
+
+	private static final String OPTION_STOP_AT_EVERY_STATEMENT = "epsilon-dap-stop-every";
 
 	private static final String DEFAULT_HOST = "localhost";
 	private static final int DEFAULT_PORT = 4040;
-	private static final String DEFAULT_PARAMETERS = "{\"request\": \"attach\"}";
+	private static final boolean DEFAULT_STOP_AT_EVERY_STATEMENT = false;
+
+	private static final Map<String, String> DEFAULT_PARAMETERS = new HashMap<>();
+	static {
+		DEFAULT_PARAMETERS.put("request", "attach");
+	}
 
 	private Text txtHost;
 	private Text txtPort;
+	private Button btnStopAtEveryStatement;
 
 	@Override
 	public void createControl(Composite parent) {
@@ -45,7 +63,7 @@ public class RemoteEpsilonConnectionConfigurationTab extends AbstractLaunchConfi
 		Composite control = new Composite(parent, SWT.NONE);
 		setControl(control);
 
-		GridLayout controlLayout = new GridLayout(3, false);
+		GridLayout controlLayout = new GridLayout(1, false);
 		control.setLayout(controlLayout);
 
 		final Group connectionGroup = createGroup(control, "Connection properties:", 2);
@@ -63,6 +81,14 @@ public class RemoteEpsilonConnectionConfigurationTab extends AbstractLaunchConfi
 		txtPort.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		txtPort.addModifyListener(this);
 
+		final Group optionsGroup = createGroup(control, "Debugging options:", 2);
+		btnStopAtEveryStatement = new Button(optionsGroup, SWT.CHECK);
+		btnStopAtEveryStatement.setText("Line breakpoints stop at every statement on the line");
+		GridData btnStopAtEveryStatementGD = new GridData(GridData.FILL_HORIZONTAL);
+		btnStopAtEveryStatementGD.horizontalSpan = 2;
+		btnStopAtEveryStatement.setLayoutData(btnStopAtEveryStatementGD);
+		btnStopAtEveryStatement.addSelectionListener(this);
+
 		control.setBounds(0, 0, 300, 300);
 		control.layout();
 		control.pack();
@@ -75,7 +101,8 @@ public class RemoteEpsilonConnectionConfigurationTab extends AbstractLaunchConfi
 		configuration.setAttribute(DSPPlugin.ATTR_DSP_SERVER_HOST, DEFAULT_HOST);
 		configuration.setAttribute(DSPPlugin.ATTR_DSP_SERVER_PORT, DEFAULT_PORT);
 		configuration.setAttribute(DSPPlugin.ATTR_DSP_MODE, DSPPlugin.DSP_MODE_CONNECT);
-		configuration.setAttribute(DSPPlugin.ATTR_DSP_PARAM, DEFAULT_PARAMETERS);
+		configuration.setAttribute(DSPPlugin.ATTR_DSP_PARAM, mapToJson(DEFAULT_PARAMETERS));
+		configuration.setAttribute(OPTION_STOP_AT_EVERY_STATEMENT, DEFAULT_STOP_AT_EVERY_STATEMENT);
 	}
 
 	@Override
@@ -83,6 +110,7 @@ public class RemoteEpsilonConnectionConfigurationTab extends AbstractLaunchConfi
 		try {
 			txtHost.setText(configuration.getAttribute(DSPPlugin.ATTR_DSP_SERVER_HOST, DEFAULT_HOST));
 			txtPort.setText(configuration.getAttribute(DSPPlugin.ATTR_DSP_SERVER_PORT, DEFAULT_PORT) + "");
+			btnStopAtEveryStatement.setSelection(configuration.getAttribute(OPTION_STOP_AT_EVERY_STATEMENT, DEFAULT_STOP_AT_EVERY_STATEMENT));
 		} catch (CoreException e) {
 			LogUtil.log(e);
 		}
@@ -93,6 +121,13 @@ public class RemoteEpsilonConnectionConfigurationTab extends AbstractLaunchConfi
 	public void performApply(ILaunchConfigurationWorkingCopy configuration) {
 		configuration.setAttribute(DSPPlugin.ATTR_DSP_SERVER_HOST, txtHost.getText());
 		configuration.setAttribute(DSPPlugin.ATTR_DSP_SERVER_PORT, Integer.parseInt(txtPort.getText()));
+		configuration.setAttribute(OPTION_STOP_AT_EVERY_STATEMENT, btnStopAtEveryStatement.getSelection());
+
+		Map<String, String> parameters = new HashMap<>(DEFAULT_PARAMETERS);
+		if (btnStopAtEveryStatement.getSelection()) {
+			parameters.put(EpsilonDebugAdapter.STOP_AT_EVERY_STATEMENT, Boolean.TRUE.toString());
+		}
+		configuration.setAttribute(DSPPlugin.ATTR_DSP_PARAM, mapToJson(parameters));
 	}
 
 	@Override
@@ -141,4 +176,25 @@ public class RemoteEpsilonConnectionConfigurationTab extends AbstractLaunchConfi
 		return EolPlugin.getDefault().createImage("icons/connect.png");
 	}
 
+	protected static String mapToJson(Map<String, String> map) {
+		List<String> entries = map.entrySet().stream()
+			.map(e -> entryToJson(e))
+			.collect(Collectors.toList());
+		return "{" + String.join(", ", entries) + "}";
+	}
+
+	protected static String entryToJson(Entry<String, String> e) {
+		return String.format("\"%s\": \"%s\"", e.getKey(), e.getValue());
+	}
+
+	@Override
+	public void widgetSelected(SelectionEvent e) {
+		canSave();
+		updateLaunchConfigurationDialog();
+	}
+
+	@Override
+	public void widgetDefaultSelected(SelectionEvent e) {
+		widgetSelected(e);
+	}
 }
